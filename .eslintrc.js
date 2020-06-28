33d62acd9fee5ec9e63778be0301988518761c13
Jonas Karlsson <jk@xdy.se>
2020-06-28 21:41:35 +0200

    build: make npm run update update foundry types, make npm run lint actually work.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
     env: {
         browser: true,
-        es6: true,
-        'jest/globals': true,
+        es6: true
     },
     extends: ['airbnb-base', 'prettier'],
     globals: {
