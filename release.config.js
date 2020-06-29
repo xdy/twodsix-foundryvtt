daddd475a5934c0a803d78ecf96e7de072046eab
Jonas Karlsson <jk@xdy.se>
2020-06-29 22:28:58 +0200

    fix: npm install after I've updated the package.json file.

--- release.config.js
+++ release.config.js
@@ -7,12 +7,16 @@ module.exports = {
             "prepareCmd": '' +
                 'sed -ie \'s|\\(.*"version"\\): "\\(.*\\)",.*|\\1: \'"\\"${nextRelease.version}\\",|" package.json ' +
                 '&& sed -ie \'s|\\(.*"version"\\): "\\(.*\\)",.*|\\1: \'"\\"${nextRelease.version}\\",|" static/system.json ' +
-                '&& sed -ie \'s|\\(.*"manifest"\\): "\\(.*\\)",.*|\\1: \'"\\"https://github.com/xdy/twodsix-foundryvtt/releases/download/v${nextRelease.version}/system.json\\",|" static/system.json' +
-                '&& sed -ie \'s|\\(.*"download"\\): "\\(.*\\)",.*|\\1: \'"\\"https://github.com/xdy/twodsix-foundryvtt/releases/download/v${nextRelease.version}/twodsix.zip\\",|" static/system.json' +
-                '&& cp static/system.json dist' +
+                '&& sed -ie \'s|\\(.*"manifest"\\): "\\(.*\\)",.*|\\1: \'"\\"https://github.com/xdy/twodsix-foundryvtt/releases/download/v${nextRelease.version}/system.json\\",|" static/system.json ' +
+                '&& sed -ie \'s|\\(.*"download"\\): "\\(.*\\)",.*|\\1: \'"\\"https://github.com/xdy/twodsix-foundryvtt/releases/download/v${nextRelease.version}/twodsix.zip\\",|" static/system.json ' +
+                '&& cp static/system.json dist ' +
+                '&& npm install ' +
                 '',
         }],
-        ["@semantic-release/git"],
+        ["@semantic-release/git", {
+            "assets": ["CHANGELOG.md", "package.json", "package-lock.json", "static/system.json"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        }],
         ["@semantic-release/github", {
             "assets": ["dist/system.json", "dist/twodsix.zip"],
         }]
