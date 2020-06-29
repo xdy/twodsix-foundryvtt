c5d325a44ceec38ddfc70a25b12f0a479525a834
Jonas Karlsson <jk@xdy.se>
2020-06-29 21:57:25 +0200

    fix: Just flailing around, really.
    Why does it work locally but not in a release build?

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,16 @@ module.exports = (env, argv) => {
         module: {
             rules: [
                 {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
                     test: /\.tsx?$/,
                     use: 'ts-loader',
                     exclude: /node_modules/,
