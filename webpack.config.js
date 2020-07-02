61508be52e9bddc8692be9cea9da3711c8e8c11c
Jonas Karlsson <jk@xdy.se>
2020-07-02 15:51:15 +0200

    fix: Sheet fixes

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = (env, argv) => {
             ),
             new WriteFilePlugin(),
             new MiniCssExtractPlugin({
-                filename: 'src/styles/twodsix.scss'
+                filename: 'styles/twodsix.css'
             })
         ],
         resolve: {
