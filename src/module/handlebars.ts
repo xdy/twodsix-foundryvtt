0dceaa06b3761bedf053a09566cf4968656be593
Jonas Karlsson <jk@xdy.se>
2020-08-04 16:51:13 +0200

    chore: Removing out some unused/redundant handlebars helpers. Better to add them if/when we need them.

--- src/module/handlebars.ts
+++ src/module/handlebars.ts
@@ -1,40 +1,4 @@
 export default function registerHandlebarsHelpers():void {
-  Handlebars.registerHelper('add', function (a, b) {
-    return a + b;
-  });
-
-  Handlebars.registerHelper('if_all', function () {
-    const args = [].slice.apply(arguments);
-    const opts = args.pop();
-
-    let {fn} = opts;
-    for (let i = 0; i < args.length; ++i) {
-      if (args[i]) continue;
-      fn = opts.inverse;
-      break;
-    }
-    return fn(this);
-  });
-
-  Handlebars.registerHelper('toLowerCase', function (str) {
-    return String.prototype.toLowerCase.call(str ?? '');
-  });
-
-  Handlebars.registerHelper('multiply', function (a, b) {
-    return a * b;
-  });
-
-  // If you need to add Handlebars helpers, here are a few useful examples:
-  Handlebars.registerHelper('concat', function () {
-    let outStr = '';
-    for (const arg in arguments) {
-      if (typeof arguments[arg] !== 'object') {
-        outStr += arguments[arg];
-      }
-    }
-    return outStr;
-  });
-
   Handlebars.registerHelper({
     add: (v1, v2) => v1 + v2,
     sub: (v1, v2) => v1 - v2,
@@ -52,26 +16,5 @@ export default function registerHandlebarsHelpers():void {
     or() {
       return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
     }
-  });
-
-  Handlebars.registerHelper('isEmpty', (element) => {
-    if (typeof element === undefined) return true;
-    if (Array.isArray(element) && element.length) return false;
-    if (element === '') return true;
-  });
-
-  Handlebars.registerHelper('enrich', (content) => {
-    return new Handlebars.SafeString(TextEditor.enrichHTML(content, {}));
-  });
-
-  Handlebars.registerHelper('option', function (value, label, selectedValue) {
-    const selectedProperty = value == selectedValue ? 'selected="selected"' : '';
-    return new Handlebars.SafeString('<option value="' + label + '"' + selectedProperty + ' data-value="' + value + '">' + label + "</option>");
-  });
-
-  Handlebars.registerHelper('select2', function (value, options) {
-    const $el = $('<select />').html(options.fn(this));
-    $el.find('[value="' + value + '"]').attr({'selected': 'selected'});
-    return $el.html();
   });
 }
