c4dccb31935bb84b5f96c187db8b7c3f3402b92c
Jonas Karlsson <jk@xdy.se>
2020-08-05 21:03:58 +0200

    refactor: Added support for changing the initiative formula, set the default to match cepheus engine.

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -9,6 +9,45 @@ export const registerSettings = function ():void {
     type: Boolean,
   });
 
-  //TODO Tons of settings to come. Initiative rule, skill-list to use, assorted rules that differ between different 2d6 rules sets (CE, CE FTL, etc)
+  const DEFAULT_INITIATIVE_FORMULA = "2d6 + @characteristics.dex.mod";
+  game.settings.register('twodsix', 'initiativeFormula', {
+    name: 'Initiative Formula',
+    hint: "Like: \"2d6 + @characteristics.dex.mod\"",
+    scope: 'world',
+    config: true,
+    default: DEFAULT_INITIATIVE_FORMULA,
+    onChange: formula => _simpleUpdateInit(formula, true)
+  });
+
+  // Retrieve and assign the initiative formula setting.
+  const initFormula = game.settings.get("twodsix", "initiativeFormula");
+  _simpleUpdateInit(initFormula);
+
+  /**
+   * Update the initiative formula.
+   * @param {string} formula - Dice formula to evaluate.
+   * @param {boolean} notify - Whether or not to post notifications.
+   */
+  function _simpleUpdateInit(formula:string, notify = false):void {
+    let message:string;
+    let notificationType:'info' | 'warning' | 'error' = "info";
+    const currentFormula = CONFIG.Combat.initiative.formula;
+    try {
+      new Roll(formula).roll();
+      message = `Set initiative formula to: ${formula}`
+    } catch (error) {
+      if (notify) {
+        message = `Failed to set initiative formula to: ${formula}, using previous value ${currentFormula} instead.`
+        notificationType = "error";
+      }
+      game.settings.set("twodsix", "initiativeFormula", currentFormula).then(() => formula = currentFormula);
+    }
+    CONFIG.Combat.initiative.formula = formula;
+    if (notify) {
+      ui.notifications.notify(message, notificationType);
+    }
+  }
+
+  //TODO Tons of settings to come. Skill-list to use, assorted rules that differ between different 2d6 rules sets (CE, CE FTL, etc)
 
 }
