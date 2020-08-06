1c4f7c413364116fc9f49521a3e2e9a64d44f17f
Jonas Karlsson <jk@xdy.se>
2020-08-06 22:40:23 +0200

    fix: Load background image from css.

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,8 @@ import fs from "fs-extra";
 import CopyWebpackPlugin from "copy-webpack-plugin";
 import WriteFilePlugin from "write-file-webpack-plugin";
 import {Configuration} from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
 
 //Only a partial type, not sure what else can be in this, and haven't looked into it.
 type FoundryConfig = { dataPath:string, systemName:string };
@@ -33,14 +35,12 @@ module.exports = (env, argv) => {
         {
           test: /\.(sa|sc|c)ss$/,
           use: [
-            'style-loader',
-            'css-loader',
+            MiniCssExtractPlugin.loader,
             {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              }
-            },],
+              loader: 'css-loader?url=false'
+            }
+          ]
+
         },
         {test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/, use: ['url-loader?limit=100000']}
       ],
@@ -54,7 +54,10 @@ module.exports = (env, argv) => {
           writeToDisk: true
         }
       ),
-      new WriteFilePlugin()
+      new WriteFilePlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'styles/twodsix.css'
+      })
     ],
     resolve: {
       extensions: ['.tsx', '.ts', '.js']
