f638f176c490d41b2875f092c309a636b019bad1
Jonas Karlsson <jk@xdy.se>
2020-08-07 20:28:23 +0200

    fix: Reformatting all files (using the current settings)
    Should be chore, but I set it to fix to force a new release, as I have also reformatted the github workflow files and want to make sure nothing broke...

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,8 +1,8 @@
-sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json \
-&& cp static/system.json dist \
-&& sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json \
-&& npm install \
-&& npm audit fix \
-&& cd dist || exit \
-&& zip -r twodsix.zip ./* \
-&& cd ..
+sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
+  cp static/system.json dist &&
+  sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
+  npm install &&
+  npm audit fix &&
+  cd dist || exit &&
+  zip -r twodsix.zip ./* &&
+  cd ..
