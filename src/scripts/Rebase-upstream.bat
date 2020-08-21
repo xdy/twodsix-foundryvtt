76be0e25a01ed7bb13483c1b9b2385aa7916012f
UltraKev <68220757+UltraKev@users.noreply.github.com>
2020-08-21 10:08:23 +0100

    fix: Fixed differences between versions

---
+++ src/scripts/Rebase-upstream.bat
@@ -0,0 +1,11 @@
+D:
+cd\GitHub\twodsix-foundryvtt
+
+git fetch upstream
+pause
+git checkout master
+pause
+git rebase upstream/master
+pause
+git push origin master --force
+pause
---
+++ src/scripts/X-Update_Branch.bat
@@ -0,0 +1,5 @@
+git checkout master
+call 1-Update.bat
+git checkout UltraKevs_Bundle_of_Sticks
+git rebase upstream/master
+pause
