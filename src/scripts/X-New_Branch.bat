816fd5d27c101c2f9f2c588aed3ef14275e2f5a4
Jonas Karlsson <jk@xdy.se>
2020-08-22 11:01:46 +0200

    chore: Updated batch files

--- src/scripts/X-New_Branch.bat
+++ src/scripts/X-New_Branch.bat
@@ -1,5 +1,8 @@
-
-cd\GitHub\twodsix-foundryvtt
-git checkout -b UltraKevs_Bundle_of_Sticks
-git push --set-upstream origin UltraKevs_Bundle_of_Sticks
+BRANCH=UltraKevs_Bundle_of_Sticks2
+echo "This creates a new branch, check to make sure that $BRANCH is the right name, and that it matches the one in X-Update_Branch.bat"
+pause
+d:
+cd \GitHub\twodsix-foundryvtt
+git checkout -b $BRANCH
+git push --set-upstream origin $BRANCH
 pause
--- src/scripts/X-Update_Branch.bat
+++ src/scripts/X-Update_Branch.bat
@@ -1,5 +1,15 @@
+BRANCH=UltraKevs_Bundle_of_Sticks2
+echo "This creates a new branch, check to make sure that $BRANCH is the right name, and that it matches the one in X-New_Branch.bat"
+pause
+D:
+cd twodsix-foundryvtt
+git status
+echo "If git status is not empty, stop and fix"
+pause
+git fetch
 git checkout master
-call 1-Update.bat
-git checkout UltraKevs_Bundle_of_Sticks
+git rebase upstream/master
+git push --force
+git checkout $BRANCH
 git rebase upstream/master
 pause
