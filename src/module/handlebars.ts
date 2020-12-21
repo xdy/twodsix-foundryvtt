4827c9b1750c74c4764098f667cfd44699d163b2
Jonas Karlsson <jk@xdy.se>
2020-12-21 13:09:29 +0100

    refactor: Adding iff to handlebars, not used yet though.

--- src/module/handlebars.ts
+++ src/module/handlebars.ts
@@ -108,4 +108,34 @@ export default function registerHandlebarsHelpers():void {
         return '0';
     }
   });
+
+  //From https://discord.com/channels/732325252788387980/732328233630171188/790507540818690068
+  //Not used yet
+  Handlebars.registerHelper("iff", function (v1, operator, v2, options) {
+    switch (operator) {
+      case '==':
+        return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+        return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '!=':
+        return (v1 != v2) ? options.fn(this) : options.inverse(this);
+      case '!==':
+        return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+        return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+        return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+        return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+        return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+        return options.inverse(this);
+    }
+  });
+
 }
