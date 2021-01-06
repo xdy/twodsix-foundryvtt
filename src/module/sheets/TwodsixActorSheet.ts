9f7652727d01729d1cd3f4ebd83000b3bdf4937d
Patrik Jonell <pjjonell@kth.se>
2021-01-06 05:55:59 +0100

    fix: change rolling
    Big change to how rolling is performed. Backward compatible, and using
    both methods for now. Not meant to be merged.
    
    Fix #212

--- src/module/sheets/TwodsixActorSheet.ts
+++ src/module/sheets/TwodsixActorSheet.ts
@@ -1,9 +1,9 @@
-import {TwodsixRolls} from "../utils/TwodsixRolls";
 import {AbstractTwodsixActorSheet} from "./AbstractTwodsixActorSheet";
 import {calcModFor} from "../utils/sheetUtils";
 import {TWODSIX} from "../config";
 import TwodsixItem from "../entities/TwodsixItem";
 import {CharacteristicType} from "../../types/twodsix";
+import { TwodsixRolls } from "../utils/TwodsixRolls";
 
 export class TwodsixActorSheet extends AbstractTwodsixActorSheet {
 
@@ -56,7 +56,13 @@ export class TwodsixActorSheet extends AbstractTwodsixActorSheet {
     super.activateListeners(html);
 
     // Rollable abilities. Really should be in base class, but that will have to wait for issue 86
+
     html.find('.rollable').on('click', (this._onRoll.bind(this)));
+
+    html.find('.perform-attack').on('click', this._onRollWrapper(this._onPerformAttack));
+    html.find('.rollable-v2').on('click', this._onRollWrapper(this._onSkillRoll));
+    html.find('.rollable-characteristic').on('click', this._onRollWrapper(this._onRollChar));
+    html.find('.rollable-untrained').on('click', this._onRollWrapper(this._onRollUntrained));
 
     html.find('.roll-damage').on('click', (this._onRollDamage.bind(this)));
 
@@ -80,6 +86,10 @@ export class TwodsixActorSheet extends AbstractTwodsixActorSheet {
     characteristic.mod = calcModFor(characteristic.current);
     await this.actor.updateActor();
   }
+  private getItem(event: Event): TwodsixItem {
+    const itemId = $(event.currentTarget).parents('.item').data('item-id');
+    return this.actor.getOwnedItem(itemId);
+  }
 
 
   /**
@@ -101,6 +111,84 @@ export class TwodsixActorSheet extends AbstractTwodsixActorSheet {
     await TwodsixRolls.performThrow(this.actor, itemId, dataset, showThrowDialog, numAttacks);
   }
 
+  private _onRollWrapper(func: (event:Event, showTrowDiag: boolean) => Promise<void>): (event:Event) => void {
+    return (event:Event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      const useInvertedShiftClick:boolean = game.settings.get('twodsix', 'invertSkillRollShiftClick');
+      const showTrowDiag = useInvertedShiftClick ? event["shiftKey"] : !event["shiftKey"];
+
+      func.bind(this)(event, showTrowDiag);
+    };
+  }
+
+  /**
+   * Handle clickable weapon attacks.
+   * @param {Event} event   The originating click event
+   * @param {boolean} showTrowDiag  Whether to show the throw dialog or not
+   * @private
+   */
+  private async _onPerformAttack(event:Event, showTrowDiag: boolean):Promise<void> {
+    const attackType = event.currentTarget["dataset"].attackType;
+    const rof = event.currentTarget["dataset"].rof ? parseInt(event.currentTarget["dataset"].rof) : null;
+
+    const item = this.getItem(event);
+    await item.performAttack(attackType, showTrowDiag, rof);
+  }
+
+  /**
+   * Handle clickable skill rolls.
+   * @param {Event} event   The originating click event
+   * @param {boolean} showTrowDiag  Whether to show the throw dialog or not
+   * @private
+   */
+  private async _onSkillRoll(event:Event, showTrowDiag: boolean):Promise<void> {
+    const item = this.getItem(event);
+    await item.skillRoll(showTrowDiag);
+  }
+
+  /**
+   * Handle clickable characteristics rolls.
+   * @param {Event} event   The originating click event
+   * @param {boolean} showTrowDiag  Whether to show the throw dialog or not
+   * @private
+   */
+  private async _onRollChar(event:Event, showTrowDiag: boolean):Promise<void> {
+    await this.actor.characteristicRoll({"characteristic": $(event.currentTarget).data("label")}, showTrowDiag);
+  }
+
+  /**
+   * Handle clickable untrained skill rolls.
+   * @param {Event} event   The originating click event
+   * @param {boolean} showTrowDiag  Whether to show the throw dialog or not
+   * @private
+   */
+  private async _onRollUntrained(event:Event, showTrowDiag: boolean):Promise<void> {
+    const data = {
+      "name": game.i18n.localize("TWODSIX.Actor.Skills.Untrained"),
+      "data": {
+        "value": game.settings.get('twodsix', 'untrainedSkillValue')
+      },
+      "type": "skills"
+    };
+
+    // this feels very hacky, but creating temporary TwodsixItems only returns a plain js object..
+    // however the rest of the handling of this roll then behaves exactly like a normal skill, so it feels
+    // somewhat worth it.
+    let skill: TwodsixItem;
+    try {
+      skill = TwodsixItem.createOwned(data, this.actor) as TwodsixItem;
+      await skill.skillRoll(showTrowDiag);
+    } finally {
+      if (skill) {
+        skill.delete();
+      }
+    }
+  }
+
+
+
   /**
    * Handle clickable damage rolls.
    * @param {Event} event   The originating click event
@@ -115,12 +203,11 @@ export class TwodsixActorSheet extends AbstractTwodsixActorSheet {
     const element = $(event.currentTarget);
     const bonusDamageFormula = String(element.data('bonus-damage') || 0);
 
-    async function extracted(this):Promise<void> {
-      return await TwodsixRolls.rollDamage(item, true, this.actor, game.settings.get('core', 'rollMode'), bonusDamageFormula);
+    async function extracted(this: TwodsixActorSheet):Promise<Roll> {
+      await TwodsixRolls.rollDamage(item, true, this.actor, game.settings.get('core', 'rollMode'), bonusDamageFormula);
+      return await item.rollDamage(game.settings.get('core', 'rollMode'), bonusDamageFormula);
     }
 
     extracted.call(this);
   }
-
-
 }
---
+++ src/module/utils/TwodsixDiceRoll.ts
@@ -0,0 +1,168 @@
+import {Crit} from "../../types/twodsix";
+import { TWODSIX } from "../config";
+import TwodsixActor from "../entities/TwodsixActor";
+import TwodsixItem from "../entities/TwodsixItem";
+import { advantageDisadvantageTerm } from "../i18n";
+import { getKeyByValue } from "./sheetUtils";
+import { TwodsixRollSettings } from "./TwodsixRollSettings";
+
+export class TwodsixDiceRoll {
+  settings: TwodsixRollSettings;
+  actor: TwodsixActor;
+  skill?: TwodsixItem;
+  item?: TwodsixItem;
+  target: number;
+  naturalTotal: number;
+  effect: number;
+  roll: Roll;
+
+  constructor (settings: TwodsixRollSettings, actor: TwodsixActor, skill: TwodsixItem=null, item: TwodsixItem=null) {
+    this.settings = settings;
+    this.actor = actor;
+    this.skill = skill;
+    this.item = item;
+
+    this.createRoll();
+
+    this.naturalTotal = this.roll.dice[0].results.reduce((total:number, dice) => {
+      return dice["active"] ? total + dice["result"] : total;
+    }, 0);
+
+    this.calculateEffect();
+  }
+
+  private createRoll(): void {
+    const showEffect = game.settings.get("twodsix", "effectOrTotal");
+    const difficultiesAsTargetNumber = game.settings.get('twodsix', 'difficultiesAsTargetNumber');
+    const rollTtype = TWODSIX.ROLLTYPES[this.settings.rollType].formula;
+    const data = {} as {string:number};
+
+    let formula = `${rollTtype}`;
+
+    // Add characteristic modifier
+    if (this.settings.characteristic !== "NONE") {
+      formula += ` + @${this.settings.characteristic}`;
+      data[this.settings.characteristic] = this.actor.getCharacteristicModifier(this.settings.characteristic);
+    }
+
+    // Add skill modifier
+    if (this.skill) {
+      formula += "+ @skill";
+      data["skill"] = this.skill.data.data.value;
+    }
+
+    // Add dice modifier
+    if (this.settings.diceModifier) {
+      formula += "+ @DM";
+      data["DM"] = this.settings.diceModifier;
+    }
+
+    // Add dificulty modifier or set target
+    if (!showEffect && !difficultiesAsTargetNumber) {
+      formula += "+ @difficultyMod";
+      data["difficultyMod"] = this.settings.difficulty.mod;
+    } else {
+      this.target = this.settings.difficulty.target;
+    }
+
+    this.roll = new Roll(formula, data).roll();
+  }
+
+  public getCrit(): Crit {
+    const CRITICAL_EFFECT_VALUE = game.settings.get('twodsix', 'absoluteCriticalEffectValue');
+    if (this.isNaturalCritSuccess()) {
+      return Crit.success;
+    } else if (this.isNaturalCritFail()) {
+      return Crit.fail;
+    } else if (this.effect >= CRITICAL_EFFECT_VALUE) {
+      return Crit.success;
+    } else if (this.effect <= -CRITICAL_EFFECT_VALUE) {
+      return Crit.fail;
+    }
+  }
+
+  private isNaturalCritSuccess():boolean {
+    return this.naturalTotal == 12;
+  }
+  private isNaturalCritFail():boolean {
+    return this.naturalTotal == 2;
+  }
+
+  public isSuccess():boolean {
+    return this.effect >= 0;
+  }
+
+  private calculateEffect():void {
+    let effect = this.target ? this.roll.total - this.target : this.roll.total;
+    if (this.isNaturalCritSuccess()) {
+      console.log(`Got a natural 2 with Effect ${effect}!`);
+      if (effect >= 0 && game.settings.get('twodsix', 'criticalNaturalAffectsEffect')) {
+        console.log("Setting Effect to -1 due to natural 2!");
+        effect = -1;
+      }
+    } else if (this.isNaturalCritFail()) {
+      console.log(`Got a natural 12 with Effect ${effect}!`);
+      if (effect < 0 && game.settings.get('twodsix', 'criticalNaturalAffectsEffect')) {
+        console.log("Setting Effect to 0 due to natural 12!");
+        effect = 0;
+      }
+    }
+    this.effect = effect;
+  }
+
+  public sendToChat():void {
+    const rollingString = game.i18n.localize("TWODSIX.Rolls.Rolling");
+    const usingString = game.i18n.localize("TWODSIX.Actor.using");
+    const resultsShowString = game.i18n.localize("TWODSIX.Rolls.resultShows");
+    const showEffect = game.settings.get("twodsix", "effectOrTotal");
+    const sumString = game.i18n.localize("TWODSIX.Rolls.sum");
+    const effectString = game.i18n.localize("TWODSIX.Rolls.Effect");
+    const difficulties = TWODSIX.DIFFICULTIES[game.settings.get('twodsix', 'difficultyListUsed')];
+    const difficulty = game.i18n.localize(getKeyByValue(difficulties, this.settings.difficulty));
+
+    let flavor = `${rollingString}: ${difficulty}`;
+
+    if (game.settings.get('twodsix', 'difficultiesAsTargetNumber')) {
+      flavor += `(${this.target}+)`;
+    }
+
+    if (this.settings.rollType != 'Normal') {
+      const rollType = advantageDisadvantageTerm(this.settings.rollType);
+      flavor += ` ${game.i18n.localize("TWODSIX.Rolls.With")} ${rollType}`;
+    }
+
+    if (this.skill) {
+      const skillValue = this.skill.data.data.value;
+      flavor += ` ${usingString} ${this.skill.data.name}(${skillValue <= 0 ? "" : "+"}${skillValue})`;
+    }
+
+    if (this.item) {
+      flavor += ` ${usingString} ${this.item.data.name}`;
+    }
+
+    if (this.roll.data.DM) {
+      flavor += ` +DM(${this.roll.data.DM <= 0 ? "" : "+"}${this.roll.data.DM})`;
+    }
+
+    if (this.settings.characteristic !== 'NONE') {
+      const characteristicValue = this.roll.data[this.settings.characteristic];
+      flavor += ` ${usingString} ${this.settings.characteristic}(${characteristicValue <= 0 ? "" : "+"}${characteristicValue})`;
+    }
+
+    flavor += ` (${resultsShowString} ${showEffect ? effectString : sumString})`;
+
+    this.roll.toMessage(
+      {
+        speaker: ChatMessage.getSpeaker({actor: this.actor}),
+        flavor: flavor,
+        rollType: this.settings.rollType,
+        flags: {
+          "core.canPopout": true,
+          "twodsix.crit": this.getCrit(),
+          "twodsix.effect": this.effect
+        }
+      },
+      {rollMode: this.settings.rollMode}
+    );
+  }
+}
