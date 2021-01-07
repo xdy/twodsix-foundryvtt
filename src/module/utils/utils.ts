f7010d52d3bf1bbf96b075e3c69d59b17710fb46
Patrik Jonell <pjjonell@kth.se>
2021-01-07 15:00:06 +0100

    fix: deal damage to unlinked tokens
    Change so that damage can be dealt to both linked  and unlinked tokens.
    The change restructures how damage is being dealt and adds a new hook for recalulating the hits when the characteristics change.
    
    Fix #232

---
+++ src/module/utils/utils.ts
@@ -0,0 +1,36 @@
+// https://stackoverflow.com/a/34749873
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ */
+export function isObject(item: unknown):boolean {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param ...sources
+ */
+export function mergeDeep(target:Record<string, any>, ...sources:Record<string, any>[]):Record<string, any> {
+  if (!sources.length) {
+    return target;
+  }
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) {
+          Object.assign(target, { [key]: {} });
+        }
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
