03403220031a409bfb8934e666e015fee54aae5c
Jonas Karlsson <jk@xdy.se>
2021-01-29 19:18:50 +0100

    fix: Release script change to hack the system.json for development pre-releases

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,5 +1,5 @@
 sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
-  if echo "$1" | grep -q "development"; then sed -i -e  "|(.*manifest.*)|\"manifest\": \"https://github.com/xdy/twodsix-foundryvtt/releases/download/v$1/system.json\"|" static/system.json; else sed -i -e  "|(.*manifest.*)|\"manifest\": \"https://github.com/xdy/twodsix-foundryvtt/releases/latest/system.json\"|" static/system.json; fi &&
+  if echo "$1" | grep -q "development"; then sed -i -e  's|(.*manifest.*)|\"manifest\": \"https://github.com/xdy/twodsix-foundryvtt/releases/download/v$1/system.json\"|' static/system.json; else sed -i -e  's|(.*manifest.*)|\"manifest\": \"https://github.com/xdy/twodsix-foundryvtt/releases/latest/system.json\"|' static/system.json; fi &&
   cp static/system.json dist &&
   sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
   npm install &&
