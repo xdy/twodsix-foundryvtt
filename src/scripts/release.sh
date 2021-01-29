aae42834fa147df2a6fa84b296c7ead8a65a0873
Jonas Karlsson <jk@xdy.se>
2021-01-29 18:31:19 +0100

    fix: Release script change to hack the system.json for development pre-releases

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,7 +1,7 @@
 sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
   cp static/system.json dist &&
   sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
-  if [[ $1 =~ "development" ]]; then sed -i -e "s|(.*)/latest/(.*)|\1/$1/\2|" system.json; fi &&
+  if echo "$1" | grep -q "development"; then sed -i -e "s|(.*)/latest/(.*)|\1/$1/\2|" system.json; fi &&
   npm install &&
   npm audit fix &&
   cd dist || exit &&
