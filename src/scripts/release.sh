5f886f71962473be6975f7ff67fe31775b841628
Patrik Jonell <pjjonell@kth.se>
2021-01-30 02:41:00 +0100

    chore: try to fix the sed stuff

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,4 +1,9 @@
+SEARCH_PATTERN='(\s\"(manifest|download)\"\: \"https:\/\/github.com\/xdy\/twodsix-foundryvtt\/releases\/).*(\/(system.json|twodsix.zip)\",)'
+DEVELOPMENT_REPLACE="\1download/v$1\3"
+MASTER_REPLACE="\1latest\3"
+
 sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
+  if echo "$1" | grep -q "development"; then sed -i -r  s"~$SEARCH_PATTERN~$DEVELOPMENT_REPLACE~" static/system.json; else sed -i -r s"~$SEARCH_PATTERN~$MASTER_REPLACE~" static/system.json; fi &&
   cp static/system.json dist &&
   sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
   npm install &&
@@ -6,6 +11,3 @@ sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
   cd dist || exit &&
   zip -r twodsix.zip ./* &&
   cd ..
-
-  # I am obviously too tired. What I want to do before line 2 is replace the manifest line with one that has the right link.
-  #if echo "$1" | grep -q "development"; then sed -i -e  's|(.*\"manifest.*)|\"manifest\": \"https://github.com/xdy/twodsix-foundryvtt/releases/download/v$1/system.json\"|' static/system.json; else sed -i -e  's|(.*\"manifest.*)|\"manifest\": \"https://github.com/xdy/twodsix-foundryvtt/releases/latest/system.json\"|' static/system.json; fi &&
