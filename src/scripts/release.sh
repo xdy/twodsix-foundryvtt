deeafbc71111515e94bd26b9e4446df157212bba
Jonas Karlsson <xdy@users.noreply.github.com>
2021-05-16 22:38:00 +0200

    fix: Ok, one more try at a release
    The problem has changed, now it's a dependency with an issue (vtt-foundry-types includes an old handlebard)
    For now, I'm removing the audit fix.

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -7,7 +7,6 @@ sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
   cp static/system.json dist &&
   sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
   npm install &&
-  npm audit fix &&
   cd dist || exit &&
   zip -r twodsix.zip ./* &&
   cd ..
