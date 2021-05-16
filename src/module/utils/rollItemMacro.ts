720ee22c56e75fa3a0ac780e3405694ca94773bc
Nick Sheck <sheck@users.noreply.github.com>
2021-05-16 02:27:35 -0700

    fix: Fix and enhance weapon hotbar macro (#489)
    * Fix and enhance weapon hotbar macro
    
    - Now rolls damage as expected
    - If using CEL rate of fire rules, prompts for attack type
    
    * Fix issues

--- src/module/utils/rollItemMacro.ts
+++ src/module/utils/rollItemMacro.ts
@@ -1,11 +1,12 @@
 import TwodsixItem from "../entities/TwodsixItem";
+import { TWODSIX } from "../config";
 
 /**
  * Use a previously created macro.
  * @param {string} itemId
  * @return {Promise}
  */
-export async function rollItemMacro(itemId:string):Promise<void> {
+export async function rollItemMacro(itemId: string): Promise<void> {
   const speaker = ChatMessage.getSpeaker();
   let actor;
   if (speaker.token) {
@@ -14,10 +15,53 @@ export async function rollItemMacro(itemId:string):Promise<void> {
   if (!actor) {
     actor = game.actors.get(speaker.actor);
   }
-  const item:TwodsixItem = actor ? actor.items.find((i) => i._id === itemId) : null;
+  const item: TwodsixItem = actor ? actor.items.find((i) => i._id === itemId) : null;
   if (!item) {
-    ui.notifications.warn(`Your controlled Actor does not have an item with ID ${itemId}`);
+    ui.notifications.warn(game.i18n.localize("TWODSIX.Warnings.ActorMissingItem").replace("_ITEM_ID_", itemId));
   } else {
-    await item.skillRoll(false);
+    if (item.data.type != "weapon") {
+      await item.skillRoll(false);
+    } else {
+      if (shouldShowCELAutoFireDialog(item)) {
+        const attackType = await promptForROF();
+        await item.performAttack(attackType, true);
+      } else {
+        await item.performAttack("", true);
+      }
+    }
   }
+}
+
+function shouldShowCELAutoFireDialog(weapon: TwodsixItem): boolean {
+  return (
+    (game.settings.get('twodsix', 'autofireRulesUsed') === TWODSIX.RULESETS.CEL.key) &&
+    (weapon.data.data.rateOfFire > 1)
+  );
+}
+
+async function promptForROF(): Promise<string> {
+  return new Promise((resolve) => {
+    new Dialog({
+      title: game.i18n.localize("TWODSIX.Dialogs.ROFPickerTitle"),
+      content: "",
+      buttons: {
+        single: {
+          label: game.i18n.localize("TWODSIX.Dialogs.ROFSingle"), callback: () => {
+            resolve('');
+          }
+        },
+        burst: {
+          label: game.i18n.localize("TWODSIX.Dialogs.ROFBurst"), callback: () => {
+            resolve('auto-burst');
+          }
+        },
+        full: {
+          label: game.i18n.localize("TWODSIX.Dialogs.ROFFull"), callback: () => {
+            resolve('auto-full');
+          }
+        }
+      },
+      default: 'single',
+    }).render(true);
+  });
 }
