bbe892c6eaa5cbe0bcedd82a38a08e02f38cfae3
Jonas Karlsson <jk@xdy.se>
2021-05-23 14:32:37 +0200

    Missed a file in the templatePath.

--- src/twodsix.ts
+++ src/twodsix.ts
@@ -81,6 +81,7 @@ Hooks.once('init', async function () {
     "systems/twodsix/templates/actors/ship-sheet.html",
     //
     "systems/twodsix/templates/actors/parts/actor/actor-characteristics.html",
+    "systems/twodsix/templates/actors/parts/actor/actor-characteristics-atom.html",
     "systems/twodsix/templates/actors/parts/actor/actor-consumable.html",
     "systems/twodsix/templates/actors/parts/actor/actor-finances.html",
     "systems/twodsix/templates/actors/parts/actor/actor-info.html",
