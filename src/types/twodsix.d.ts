efd806b32693d4d3376b1beb98778d1ebb890c9d
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2021-09-06 13:12:17 -0500

    fix: typos for traits

--- src/types/twodsix.d.ts
+++ src/types/twodsix.d.ts
@@ -1,5 +1,5 @@
 //MUST match what's in the template.json (or, at least not contradict it). TODO Should build this from the template.json I guess
-export type TwodsixItemType = "equipment" | "weapon" | "armor" | "augment" | "storage" | "tool" | "junk" | "skills" | "consumable";
+export type TwodsixItemType = "equipment" | "weapon" | "armor" | "augment" | "storage" | "tool" | "junk" | "skills" | "trait"| "consumable";
 
 // @ts-ignore
 export interface TwodsixItemData extends ItemData {
