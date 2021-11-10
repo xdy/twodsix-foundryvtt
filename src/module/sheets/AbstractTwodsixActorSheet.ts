1bf2226115c9ead0e6a5763c27d3026baab64864
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2021-11-10 13:04:54 -0600

    fix: restrict the item types that can be dropped on actor sheet
    System currently doesn't restrict drops by item types.  For example, a skill item can be dropped on a ship sheet. This commit restricts item drops that make sense, i.e. no components on travellers, and no skills, augments, or trains on ships.

--- src/module/sheets/AbstractTwodsixActorSheet.ts
+++ src/module/sheets/AbstractTwodsixActorSheet.ts
@@ -154,6 +154,7 @@ export abstract class AbstractTwodsixActorSheet extends ActorSheet {
     event.preventDefault();
 
     const data = getDataFromDropEvent(event);
+    const actor = this.actor;
 
     if (!data) {
       console.log(`Twodsix | Dragging something that can't be dragged`);
@@ -161,22 +162,35 @@ export abstract class AbstractTwodsixActorSheet extends ActorSheet {
     }
 
     if (data.type === 'damageItem') {
-      const useInvertedShiftClick: boolean = (<boolean>game.settings.get('twodsix', 'invertSkillRollShiftClick'));
-      const showDamageDialog = useInvertedShiftClick ? event["shiftKey"] : !event["shiftKey"];
-      // @ts-ignore
-      await this.actor.damageActor(data.payload["damage"], showDamageDialog);
-      return;
+      if (actor.data.type === 'traveller') {
+        const useInvertedShiftClick: boolean = (<boolean>game.settings.get('twodsix', 'invertSkillRollShiftClick'));
+        const showDamageDialog = useInvertedShiftClick ? event["shiftKey"] : !event["shiftKey"];
+        // @ts-ignore
+        await this.actor.damageActor(data.payload["damage"], showDamageDialog);
+      } else {
+        ui.notifications.warn(game.i18n.localize("TWODSIX.Warnings.CantAutoDamageShip"));
+      }
+      return false;
     }
 
-    const actor = this.actor;
     const itemData = await getItemDataFromDropData(data);
 
-
-    if (itemData.type === 'skills') {
-      return this.handleDroppedSkills(actor, itemData, data, event);
-    } else {
-      return this.handleDroppedItem(actor, itemData, data, event);
+    switch (actor.data.type) {
+      case 'traveller':
+        if (itemData.type === 'skills') {
+          return this.handleDroppedSkills(actor, itemData, data, event);
+        } else if (!["component"].includes(itemData.type)) {
+          return this.handleDroppedItem(actor, itemData, data, event);
+        }
+        break;
+      case 'ship':
+        if (!["augment", "skills", "trait"].includes(itemData.type)) {
+          return this.handleDroppedItem(actor, itemData, data, event);
+        }
+        break;
     }
+    ui.notifications.warn(game.i18n.localize("TWODSIX.Warnings.CantDragOntoActor"));
+    return false;
   }
 
   private async handleDroppedSkills(actor: ActorSheet.Data<Actor> extends ActorSheet.Data<infer T> ? T : Actor, itemData: TwodsixItemData, data: Record<string, any>, event: DragEvent) {
