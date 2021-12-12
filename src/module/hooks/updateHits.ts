d20df410cb50f34252cde6776f1c7b5dabc1bfeb
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2021-12-12 10:49:22 -0600

    feat: add optional status indicators for wounds per CD rules

--- src/module/hooks/updateHits.ts
+++ src/module/hooks/updateHits.ts
@@ -33,3 +33,74 @@ Hooks.on('preUpdateToken', async (scene, token:Record<string, any>, update:Recor
     );
   }
 });
+
+Hooks.on('updateActor', async (actor:TwodsixActor, update:Record<string, any>) => {
+  if (update.data?.characteristics?.lifeblood && game.settings.get('twodsix', 'useCDWoundedStatusIndicators')){
+    if (actor.isToken) {
+      applyWoundedEffect(canvas.tokens.ownedTokens.find(t => t.id === actor.token.id));
+    } else {
+      applyWoundedEffect(canvas.tokens.ownedTokens.find(t => t.data.actorId === actor.id));
+    }
+  }
+});
+
+Hooks.on('updateToken', async (scene, token:Record<string, any>, update:Record<string, any>) => {
+  if (update.actorData?.data?.characteristics?.lifeblood && game.settings.get('twodsix', 'useCDWoundedStatusIndicators')) {
+    applyWoundedEffect(canvas.tokens.ownedTokens.find(t => t.id === token._id));
+  }
+});
+
+async function applyWoundedEffect (selectedToken:Record<string, any>):Promise<void> {
+  const minorWoundTint = '#FFFF00' // Yellow
+  const seriousWoundTint = '#FF0000' // Red
+  const deadTint = '#FFFFFF' // White
+
+  let tintToApply: string;
+  if (selectedToken.actor.data.data.characteristics.lifeblood.current < selectedToken.actor.data.data.characteristics.lifeblood.value) {
+    tintToApply = minorWoundTint;
+  }
+  if (selectedToken.actor.data.data.characteristics.lifeblood.current < (selectedToken.actor.data.data.characteristics.lifeblood.value / 2)) {
+    tintToApply = seriousWoundTint;
+  }
+  if (selectedToken.actor.data.data.characteristics.lifeblood.current === 0) {
+    tintToApply = deadTint;
+  }
+
+  const woundedEffectLabel = 'Bleeding';
+  const deadEffectLabel = 'Dead';
+  const unconsciousEffectLabel = 'Unconscious';
+
+  if (!tintToApply) {
+    setEffectState(deadEffectLabel, selectedToken, false);
+    setEffectState(woundedEffectLabel, selectedToken, false);
+  } else {
+    if (tintToApply === deadTint) {
+      setEffectState(deadEffectLabel, selectedToken, true);
+      setEffectState(woundedEffectLabel, selectedToken, false);
+      setEffectState(unconsciousEffectLabel, selectedToken, false);
+    } else {
+      setEffectState(deadEffectLabel, selectedToken, false);
+      const oldWoundState = await selectedToken.actor.data.effects.find(eff => eff.data.label === woundedEffectLabel);
+      const isAlreadyUnconscious = await selectedToken.actor.data.effects.find(eff => eff.data.label === unconsciousEffectLabel);
+
+      if (oldWoundState?.data.tint !== seriousWoundTint && tintToApply === seriousWoundTint && !isAlreadyUnconscious) {
+        const returnRoll = await selectedToken.actor.characteristicRoll({ characteristic: 'END', difficulty: { mod: 0, target: 8 } }, false);
+        if (returnRoll.effect < 0) {
+          await setEffectState(unconsciousEffectLabel, selectedToken, true);
+        }
+      }
+
+      await setEffectState(woundedEffectLabel, selectedToken, true);
+      const newEffect = await selectedToken.actor.data.effects.find(eff => eff.data.label === woundedEffectLabel);
+      await selectedToken.actor.updateEmbeddedDocuments('ActiveEffect', [{ _id: newEffect.id, tint: tintToApply }]);
+    }
+  }
+}
+
+async function setEffectState (effectLabel: string, targetToken:Record<string, any>, state: boolean):Promise<void> {
+  const isAlreadySet = await targetToken.actor.effects.find(eff => eff.data.label === effectLabel);
+  if ((typeof isAlreadySet !== 'undefined') !== state) {
+    const targetEffect = await CONFIG.statusEffects.find(effect => (effect.id === effectLabel.toLowerCase()));
+    await targetToken.toggleEffect(targetEffect);
+  }
+}
