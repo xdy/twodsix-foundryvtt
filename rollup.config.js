7343683810aded1c704616e87086e9b614cbe8ed
Patrik Jonell <me@patrikjonell.se>
2021-12-23 16:28:57 -0500

    feat: dynamically imports in rollup
    Previously when using webpack it was possible to import files
    dynamically, which enables migrations, import of all hooks and
    templates. This change adds this functionallity back to rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,34 @@ const commonjs = require("@rollup/plugin-commonjs");
 
 const {nodeResolve} = require('@rollup/plugin-node-resolve');
 const esbuild = require('rollup-plugin-esbuild');
+const dynamicImportVars = require('@rollup/plugin-dynamic-import-vars');
+const fs = require("fs");
+const glob = require("glob");
+const plugins = [
+  nodeResolve(),
+  commonjs(),
+  esbuild.default({
+    include: /\.[jt]sx?$/, // TODO Might have to include d.ts here
+    sourceMap: true,
+    minify: process.env.NODE_ENV === 'production',
+  }),
+  dynamicImportVars.default({}),
+];
 
-module.exports = {
-  input: 'src/twodsix.ts',
-  output: {
-    file: 'dist/twodsix.bundle.js',
-    format: 'es',
-    sourcemap: true,
-  },
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    esbuild.default({
-      include: /\.[jt]sx?$/, // TODO Might have to include d.ts here
-      sourceMap: true,
-      minify: process.env.NODE_ENV === 'production',
-    }),
-  ],
-};
+module.exports = function() {
+  const migrations = JSON.stringify(fs.readdirSync("src/migrations").map(name => name.slice(0, -3)));
+  const hooks = JSON.stringify(fs.readdirSync('src/module/hooks').map(name => name.slice(0, -3)));
+  const templates = JSON.stringify(glob.sync('static/templates/**/*.html').map(file => file.replace("static", "systems/twodsix")));
+  
+  return {
+    input: 'src/twodsix.ts',
+    output: {
+      file: 'dist/twodsix.bundle.js',
+      format: 'es',
+      sourcemap: true,
+      inlineDynamicImports: true,
+      intro: `const migrationFileNames = ${migrations};\nconst handlebarsTemplateFiles = ${templates};\nconst hookScriptFiles = ${hooks};`
+    },
+    plugins: plugins,
+  };
+}
--- src/module/migration.ts
+++ src/module/migration.ts
@@ -1,27 +1,15 @@
-
-
-const migrations = {};
-
-// this is a way of importing att of the files in the migrations folder and assign them to a property of an object.
-// require.context("../migrations", false, /\.ts$/).keys().forEach((fileName => {
-//   const newFileName = fileName.substring(2);
-//   import("../migrations/" + newFileName).then((migration) => {
-//     migrations[newFileName.split("-")[0]] = migration;
-//   });
-// }));
-
-
 export default async function migrateWorld(version:string):Promise<void> {
-  console.log("MIGRATIONS ARE BROKEN due to switch to rollup.");
-  console.log("TODO Look into https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars to fix this.");
-  await Promise.all(Object.keys(migrations).sort().map(async migrationName => {
+  //@ts-ignore
+  await migrationFileNames.sort().reduce(async (prev, migrationName) => {
+    await prev;
     if (migrationName > version) {
+      const migration = await import(`../migrations/${migrationName}.ts`);
       console.log("Migrating", migrationName);
-      await migrations[migrationName].migrate();
+      await migration.migrate();
       console.log("Done migrating", migrationName);
       await game.settings.set("twodsix", "systemMigrationVersion", migrationName);
     }
-  }));
+  }, Promise.resolve());
   console.log("Done with all migrations");
 }
 
