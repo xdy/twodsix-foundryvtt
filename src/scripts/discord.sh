4a1e76cde33c3c5feaf551243368e374e446682c
Patrik Jonell <me@patrikjonell.se>
2022-01-03 15:06:31 -0500

    update to discord script (#779)

--- src/scripts/discord.sh
+++ src/scripts/discord.sh
@@ -1,11 +1,9 @@
 # Inspired by https://github.com/DiscordHooks/github-actions-discord-webhook
 
-AUTHOR_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%aN")"
 COMMIT_MESSAGE="$(git log -1 "$GITHUB_SHA" --pretty="%B" | sed 's/\r$//')" 
 VERSION="$(git describe --tags --abbrev=0)"
 
 # local debug
-# AUTHOR_NAME="$(git log -1 --skip 1 --pretty="%ae")"
 # COMMIT_MESSAGE="$(git log -1 --skip 1 --pretty="%B" | sed 's/\r$//')" 
 # VERSION="$(git describe --tags --abbrev=0)"
 
@@ -18,5 +16,10 @@ WEBHOOK_DATA="{
   \"content\": \"${MESSAGE//$'\n'/\\n}\"
 }"
 
-echo -e "[Webhook]: Sending webhook to Discord...\\n";
-(curl --fail --progress-bar -A "GitHub-Actions-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "${WEBHOOK_DATA}" "$1" && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook." 
+REGEX="^(feat|fix):|BREAKING CHANGE"
+if [[ $COMMIT_MESSAGE =~ $REGEX ]];
+then
+    echo -e "[Webhook]: Sending webhook to Discord...\\n";
+    (curl --fail --progress-bar -A "GitHub-Actions-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "${WEBHOOK_DATA}" "$1" && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook." 
+fi
+
