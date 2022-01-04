15180828ac7a48062d3dc14a7f62230867a48827
Jonas Karlsson <jk@xdy.se>
2022-01-04 10:41:39 +0100

    refactor: Change to always use the tag rather than latest

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,9 +1,7 @@
 SEARCH_PATTERN='(\s\"(manifest|download)\"\: \"https:\/\/github.com\/xdy\/twodsix-foundryvtt\/releases\/).*(\/(system.json|twodsix.zip)\",)'
 DEVELOPMENT_REPLACE="\1download/v$1\3"
-MASTER_REPLACE="\1latest/download\3"
 
-sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
-  if echo "$1" | grep -q "foundry-0.81"; then sed -i -r  s"~$SEARCH_PATTERN~$DEVELOPMENT_REPLACE~" static/system.json; else sed -i -r s"~$SEARCH_PATTERN~$MASTER_REPLACE~" static/system.json; fi &&
+  sed -i -r  s"~$SEARCH_PATTERN~$DEVELOPMENT_REPLACE~" static/system.json &&
   cp static/system.json dist &&
   sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
   npm install &&
