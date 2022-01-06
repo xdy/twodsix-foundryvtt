d2d85ee4231fa785b1fac01b571277110a9da640
Jonas Karlsson <xdy@users.noreply.github.com>
2022-01-06 20:38:21 +0100

    fix: Try to fix release by reverting...
    Revert "refactor: Change to always use the tag rather than latest"
    
    This reverts commit 15180828ac7a48062d3dc14a7f62230867a48827.

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,7 +1,9 @@
 SEARCH_PATTERN='(\s\"(manifest|download)\"\: \"https:\/\/github.com\/xdy\/twodsix-foundryvtt\/releases\/).*(\/(system.json|twodsix.zip)\",)'
 DEVELOPMENT_REPLACE="\1download/v$1\3"
+MASTER_REPLACE="\1latest/download\3"
 
-  sed -i -r  s"~$SEARCH_PATTERN~$DEVELOPMENT_REPLACE~" static/system.json &&
+sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
+  if echo "$1" | grep -q "foundry-0.81"; then sed -i -r  s"~$SEARCH_PATTERN~$DEVELOPMENT_REPLACE~" static/system.json; else sed -i -r s"~$SEARCH_PATTERN~$MASTER_REPLACE~" static/system.json; fi &&
   cp static/system.json dist &&
   sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
   npm install &&
