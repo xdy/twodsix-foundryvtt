b0356ff09baa819a4fdb36dd2e58f1a5b3b9add5
Patrik Jonell <me@patrikjonell.se>
2022-01-07 11:13:59 +0100

    updated migration

--- src/migrations/2022_01_03_05_08_25_active-effects-for-traits.ts
+++ src/migrations/2022_01_03_05_08_25_active-effects-for-traits.ts
@@ -1,21 +1,41 @@
-async function applyActiveEffects(p, actor) {
-  await p;
-  return Promise.all(actor.items.filter(item => item.type === "trait").map(async item => {
-    if (!item.data.data.effectId) {
+import TwodsixActor from "src/module/entities/TwodsixActor";
+import Trait = dataTwodsix.Trait;
+
+async function applyActiveEffects(actor:TwodsixActor) {
+  for (const item of actor.items.filter((itm:TwodsixItem) => itm.type === "trait")) {
+    if (!(<Trait>item.data.data).effectId) {
       const effects = await actor.createEmbeddedDocuments("ActiveEffect", [{
         origin: item.uuid,
         icon: item.img
       }]);
       await item.update({ "data.effectId": effects[0].id });
     }
-    return Promise.resolve();
-  }));
+  }
+  return Promise.resolve();
 }
 
 export async function migrate(): Promise<void> {
-  const tokenActor = game.scenes?.reduce((memo: TokenDocument[], scene: Scene) => memo.concat(scene.tokens.contents), [])
-    .filter((token: TokenDocument) => token.actor?.type === "traveller" && !token.data.actorLink)
-    .map((token: TokenDocument) => token.actor);
-  await game.actors?.filter(actor => actor.type === "traveller").reduce(applyActiveEffects, Promise.resolve());
-  await tokenActor?.reduce(applyActiveEffects, Promise.resolve());
+  const allActors = game.actors?.filter(actor => actor.type === 'traveller') as unknown as TwodsixActor[];
+	
+	for (const scene of game.scenes ?? []) {
+		for (const token of scene.tokens ?? []) {
+			if (token.actor && !token.data.actorLink && token.actor.type === 'traveller') {
+				allActors.push(token.actor as TwodsixActor);
+			}
+		}
+	}
+
+	// @ts-expect-error The type definitions for metadata must be updated to support "type"
+	const actorPacks = game.packs.filter(pack => pack.metadata.type === 'Actor' && !pack.locked);
+	for (const pack of actorPacks) {
+    for (const actor of await pack.getDocuments()) {
+      allActors.push(actor as unknown as TwodsixActor);
+    }
+	}
+
+	for (const actor of allActors) {
+		await applyActiveEffects(actor);
+	}
+
+	return Promise.resolve();
 }
