b52d8e725c2819bb49711a181c82645b285da4bc
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-01-29 12:38:23 -0600

    feat: show difficulty values along with text (#848)
    When prompted for a skill roll, the difficulty number (either target or modifier - as determined in settings) is shown after difficulty name in menu pull down.

--- src/module/handlebars.ts
+++ src/module/handlebars.ts
@@ -13,6 +13,10 @@ export default function registerHandlebarsHelpers(): void {
     return advantageDisadvantageTerm(str);
   });
 
+  Handlebars.registerHelper('twodsix_difficultiesAsTargetNumber', () => {
+    return game.settings.get('twodsix', 'difficultiesAsTargetNumber');
+  });
+
   Handlebars.registerHelper('twodsix_isOdd', (num:number) => {
     return (num % 2) == 1;
   });
