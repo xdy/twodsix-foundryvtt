6a90275a1d1fcc89c78e36f2e06ed13a09d684af
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-02-14 14:30:25 -0600

    force cut and paste for contenteditables to be plain text

--- src/module/sheets/AbstractTwodsixItemSheet.ts
+++ src/module/sheets/AbstractTwodsixItemSheet.ts
@@ -5,6 +5,10 @@ export abstract class AbstractTwodsixItemSheet extends ItemSheet {
       'focusout',
       this._onSubmit.bind(this)
     );
+    html.find('div[contenteditable="true"][data-edit]').on(
+      'paste',
+      onPasteStripFormatting.bind(this)
+    );
   }
 
 
@@ -18,5 +22,17 @@ export abstract class AbstractTwodsixItemSheet extends ItemSheet {
     data.data.owner = this.actor;
 
     return data;
+  }
+}
+
+export function onPasteStripFormatting(event): void {
+  if (event.originalEvent && event.originalEvent.clipboardData && event.originalEvent.clipboardData.getData) {
+    event.preventDefault();
+    const text = event.originalEvent.clipboardData.getData('text/plain');
+    window.document.execCommand('insertText', false, text);
+  } else if (event.clipboardData && event.clipboardData.getData) {
+    event.preventDefault();
+    const text = event.clipboardData.getData('text/plain');
+    window.document.execCommand('insertText', false, text);
   }
 }
