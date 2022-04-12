1569a41fa9b5c09979b2fa1651645b2279c2c847
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-04-12 16:31:32 -0500

    feat: allow damage drop on tokens

---
+++ src/module/hooks/dropDamageOnToken.ts
@@ -0,0 +1,42 @@
+//Liberally adapted from "hey-catch"
+import TwodsixActor from "../entities/TwodsixActor";
+Hooks.on('dropCanvasData', catchDrop);
+
+async function catchDrop(canvasObject: Canvas, dropData) {
+
+  if (dropData.type === 'damageItem') {
+    const { x, y } = dropData;
+
+    // Reference used: PlaceablesLayer.selectObjects
+    // Find token(s) at drop location
+    const controllable = canvasObject.tokens?.placeables.filter(obj => obj.visible && obj.actor && obj.control instanceof Function);
+    const found = controllable?.filter(obj => {
+      const w = obj.width, h = obj.height;
+      return Number.between(x, obj.x, obj.x + w) && Number.between(y, obj.y, obj.y + h);
+    });
+
+    if (found?.length === 0 || !found) {
+      return ui.notifications?.info('No target for item drop found.');
+    } else if (found.length === 1) {
+      //console.log('Dropped On:', found[0]);
+      const target = found[0];
+
+      const actor = target?.actor;
+      console.log(actor);
+
+      // TODO: Could add support for asking GM to validate drop onto unowned actor?
+      if (!actor?.isOwner) {
+        return ui.notifications?.warn('You lack sufficient permissions on target actor.');
+      }
+      if (actor.data.type === 'traveller') {
+        await (<TwodsixActor>actor).damageActor(dropData.payload.damage, dropData.payload.armorPiercingValue, true);
+      } else {
+        ui.notifications.warn(game.i18n.localize("TWODSIX.Warnings.CantAutoDamageShip"));
+      }
+      return false;
+    } else if (found.length > 1) {
+      // Make sure only one token is there to avoid mistakes
+      return ui.notifications?.warn('Multiple actors found.');
+    }
+  }
+}
