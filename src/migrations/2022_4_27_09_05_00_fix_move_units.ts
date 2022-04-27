cd2df3e7106fdc67c1e02061ccf8898a6eb0d233
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-04-27 14:19:04 -0500

    fix: movement units issue

---
+++ src/migrations/2022_4_27_09_05_00_fix_move_units.ts
@@ -0,0 +1,34 @@
+import { Traveller } from "../types/template";
+import { applyToAllActors } from "../migration-utils";
+
+async function adjustMovementUnits (actor: TwodsixActor): Promise<void> {
+  const actorData = actor.data.data as Traveller;
+  if (actorData.movement?.units) {
+    console.log("Starting units: ", actorData.movement.units);
+    switch (actorData.movement?.units) {
+      case 'TWODSIX.Actor.Movement.DistFt':
+        await actor.update({ 'data.movement.units': 'ft' });
+        break;
+      case 'TWODSIX.Actor.Movement.DistMi':
+        await actor.update({ 'data.movement.units': 'mi' });
+        break;
+      case 'TWODSIX.Actor.Movement.DistM':
+        await actor.update({ 'data.movement.units': 'm' });
+        break;
+      case 'TWODSIX.Actor.Movement.DistKm':
+        await actor.update({ 'data.movement.units': 'km' });
+        break;
+      default:
+        console.log('nothing changed');
+        break;
+    }
+    console.log("Ending Units: ", actorData.movement?.units);
+  }
+  return Promise.resolve();
+}
+
+export async function migrate(): Promise<void> {
+  await applyToAllActors(adjustMovementUnits);
+
+  return Promise.resolve();
+}
