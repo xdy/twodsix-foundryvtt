64164fcd7eabd62b8e234a38c35a1937b7067e19
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-05-06 08:18:54 -0500

    Update template.d.ts

--- src/types/template.d.ts
+++ src/types/template.d.ts
@@ -335,6 +335,7 @@ export interface Armor extends GearTemplate {
   type:string;
   useConsumableForAttack:string;
   location:string[];
+  isPowered:boolean;
 }
 
 export interface Augment extends GearTemplate {
