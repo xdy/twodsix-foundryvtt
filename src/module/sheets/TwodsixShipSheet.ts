55e489787333647d634f2ff29cf1c85fb8a3e683
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-05-07 13:44:09 -0500

    fix null ship positions

--- src/module/sheets/TwodsixShipSheet.ts
+++ src/module/sheets/TwodsixShipSheet.ts
@@ -13,25 +13,27 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
     const context = <TwodsixShipSheetData>super.getData();
     context.dtypes = ["String", "Number", "Boolean"];
     AbstractTwodsixActorSheet._prepareItemContainers(this.actor.items, context);
-
-    context.shipPositions = this.actor.items.filter((item:TwodsixItem)=>item.type==="ship_position").map((shipPosition:TwodsixItem) => {
-      const shipPositionActorIds = Object.entries(<ShipPositionActorIds>(<Ship>this.actor.data.data).shipPositionActorIds).filter(([, shipPositionId]) => shipPositionId === shipPosition.id);
-      if (shipPositionActorIds.length > 0) {
-        const actorIds = shipPositionActorIds.map(([actorId,]) => actorId);
-        (<ShipPosition>shipPosition.data.data).actors = <TwodsixActor[]>actorIds.map(actorId => game.actors?.get(actorId)).filter(x => x !== undefined);
-      } else {
-        (<ShipPosition>shipPosition.data.data).actors = [];
-      }
-      const actions = (<ShipPosition>shipPosition.data.data).actions ?? [];
-      (<ShipPosition>shipPosition.data.data).sortedActions = Object.entries(actions).map(([id, ret]) => {
-        ret.id = id;
-        return ret;
+    if ((<Ship>this.actor.data.data).shipPositionActorIds) {
+      context.shipPositions = this.actor.items.filter((item: TwodsixItem) => item.type === "ship_position").map((shipPosition: TwodsixItem) => {
+        const shipPositionActorIds = Object?.entries(<ShipPositionActorIds>(<Ship>this.actor.data.data).shipPositionActorIds)?.filter(([, shipPositionId]) => shipPositionId === shipPosition.id);
+        if (shipPositionActorIds.length > 0) {
+          const actorIds = shipPositionActorIds.map(([actorId,]) => actorId);
+          (<ShipPosition>shipPosition.data.data).actors = <TwodsixActor[]>actorIds.map(actorId => game.actors?.get(actorId)).filter(x => x !== undefined);
+        } else {
+          (<ShipPosition>shipPosition.data.data).actors = [];
+        }
+        const actions = (<ShipPosition>shipPosition.data.data).actions ?? [];
+        (<ShipPosition>shipPosition.data.data).sortedActions = Object.entries(actions).map(([id, ret]) => {
+          ret.id = id;
+          return ret;
+        });
+        (<ShipPosition>shipPosition.data.data).sortedActions?.sort((a, b) => (a.order > b.order) ? 1 : -1);
+        return shipPosition;
       });
-      (<ShipPosition>shipPosition.data.data).sortedActions?.sort((a, b) => (a.order > b.order) ? 1 : -1);
-      return shipPosition;
-    });
-    context.shipPositions.sort((a:TwodsixItem,b:TwodsixItem) => (<ShipPosition>a.data.data).order-(<ShipPosition>b.data.data).order);
-
+      context.shipPositions.sort((a: TwodsixItem, b: TwodsixItem) => (<ShipPosition>a.data.data).order - (<ShipPosition>b.data.data).order);
+    } else {
+      context.shipPositions = [];
+    }
     context.settings = <TwodsixShipSheetSettings>{
       showSingleComponentColumn: game.settings.get('twodsix', 'showSingleComponentColumn')
     };
