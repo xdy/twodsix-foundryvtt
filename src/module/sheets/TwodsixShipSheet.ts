9911451a62e2d88e74b009b7e01b66dfbaa3b314
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-07-08 13:48:18 -0500

    Update TwodsixShipSheet.ts

--- src/module/sheets/TwodsixShipSheet.ts
+++ src/module/sheets/TwodsixShipSheet.ts
@@ -1,6 +1,6 @@
 import { TwodsixShipSheetData, TwodsixShipSheetSettings } from "../../types/twodsix";
 import { ShipPosition, ShipPositionActorIds, Ship, Component } from "../../types/template";
-import { getDataFromDropEvent } from "../utils/sheetUtils";
+import { getDataFromDropEvent, getItemDataFromDropData } from "../utils/sheetUtils";
 import { TwodsixShipActions } from "../utils/TwodsixShipActions";
 import { AbstractTwodsixActorSheet } from "./AbstractTwodsixActorSheet";
 import { TwodsixShipPositionSheet } from "./TwodsixShipPositionSheet";
@@ -13,24 +13,24 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
     const context = <TwodsixShipSheetData>super.getData();
     context.dtypes = ["String", "Number", "Boolean"];
     AbstractTwodsixActorSheet._prepareItemContainers(this.actor.items, context);
-    if ((<Ship>this.actor.data.data).shipPositionActorIds) {
+    if ((<Ship>this.actor.system).shipPositionActorIds) {
       context.shipPositions = this.actor.items.filter((item: TwodsixItem) => item.type === "ship_position").map((shipPosition: TwodsixItem) => {
-        const shipPositionActorIds = Object?.entries(<ShipPositionActorIds>(<Ship>this.actor.data.data).shipPositionActorIds)?.filter(([, shipPositionId]) => shipPositionId === shipPosition.id);
+        const shipPositionActorIds = Object?.entries(<ShipPositionActorIds>(<Ship>this.actor.system).shipPositionActorIds)?.filter(([, shipPositionId]) => shipPositionId === shipPosition.id);
         if (shipPositionActorIds?.length > 0) {
           const actorIds = shipPositionActorIds.map(([actorId,]) => actorId);
-          (<ShipPosition>shipPosition.data.data).actors = <TwodsixActor[]>actorIds.map(actorId => game.actors?.get(actorId)).filter(x => x !== undefined);
+          (<ShipPosition>shipPosition.system).actors = <TwodsixActor[]>actorIds.map(actorId => game.actors?.get(actorId)).filter(x => x !== undefined);
         } else {
-          (<ShipPosition>shipPosition.data.data).actors = [];
+          (<ShipPosition>shipPosition.system).actors = [];
         }
-        const actions = (<ShipPosition>shipPosition.data.data).actions ?? [];
-        (<ShipPosition>shipPosition.data.data).sortedActions = Object.entries(actions).map(([id, ret]) => {
+        const actions = (<ShipPosition>shipPosition.system).actions ?? [];
+        (<ShipPosition>shipPosition.system).sortedActions = Object.entries(actions).map(([id, ret]) => {
           ret.id = id;
           return ret;
         });
-        (<ShipPosition>shipPosition.data.data).sortedActions?.sort((a, b) => (a.order > b.order) ? 1 : -1);
+        (<ShipPosition>shipPosition.system).sortedActions?.sort((a, b) => (a.order > b.order) ? 1 : -1);
         return shipPosition;
       });
-      context.shipPositions.sort((a: TwodsixItem, b: TwodsixItem) => (<ShipPosition>a.data.data).order - (<ShipPosition>b.data.data).order);
+      context.shipPositions.sort((a: TwodsixItem, b: TwodsixItem) => (<ShipPosition>a.system).order - (<ShipPosition>b.system).order);
     } else {
       context.shipPositions = [];
     }
@@ -80,7 +80,7 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
       const actionId = $(event.currentTarget).data("id");
       const shipPositionId = $(event.currentTarget).parents(".ship-position").data("id");
       const shipPosition = this.actor.items.get(shipPositionId);
-      const action = (<ShipPosition>shipPosition?.data?.data)?.actions[actionId];
+      const action = (<ShipPosition>shipPosition?.system)?.actions[actionId];
       if (action) {
         const extra = {
           actor: game.actors?.get(actorId),
@@ -123,10 +123,10 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
       // get rid of missing actors
       if (this.actor) {
         const shipActor = <TwodsixActor>this.actor;
-        for (const actorId in (<Ship>shipActor.data.data)?.shipPositionActorIds) {
+        for (const actorId in (<Ship>shipActor.system)?.shipPositionActorIds) {
           const actor = game.actors?.get(actorId);
           if (actor === undefined) {
-            await shipActor.update({[`data.shipPositionActorIds.-=${actorId}`]: null });
+            await shipActor.update({[`system.shipPositionActorIds.-=${actorId}`]: null });
           }
         }
       }
@@ -142,10 +142,10 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
     })) {
       const shipPositionId = $(event.currentTarget).parents(".ship-position").data("id");
 
-      (<ShipPosition>(<TwodsixItem>this.actor.items.get(shipPositionId)).data.data).actors?.forEach((actor:TwodsixActor) => {
-        if (actor.id && actor.id in (<Ship>this.actor.data.data).shipPositionActorIds) {
+      (<ShipPosition>(<TwodsixItem>this.actor.items.get(shipPositionId)).system).actors?.forEach((actor:TwodsixActor) => {
+        if (actor.id && actor.id in (<Ship>this.actor.system).shipPositionActorIds) {
           if (actor.id) {
-            this.actor.update({ [`data.shipPositionActorIds.-=${actor.id}`]: null });
+            this.actor.update({ [`system.shipPositionActorIds.-=${actor.id}`]: null });
           }
         }
       });
@@ -168,23 +168,23 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
       const li = $(event.currentTarget).parents(".item");
       const itemSelected = this.actor.items.get(li.data("itemId"));
       const stateTransitions = {"operational": "damaged", "damaged": "destroyed", "destroyed": "off", "off": "operational"};
-      itemSelected?.update({"data.status": stateTransitions[(<Component>itemSelected.data.data)?.status]});
+      itemSelected?.update({"system.status": stateTransitions[(<Component>itemSelected.system)?.status]});
     }
   }
 
   private _onAdjustFuelType() {
-    this.actor.update({"data.shipStats.fuel.isRefined": !(<Ship>this.actor.data.data).shipStats.fuel.isRefined});
+    this.actor.update({"system.shipStats.fuel.isRefined": !(<Ship>this.actor.system).shipStats.fuel.isRefined});
   }
 
   private _onDeckplanClick() {
-    if ((<Ship>this.actor.data.data)?.deckPlan) {
-      game.scenes?.get((<Ship>this.actor.data.data).deckPlan)?.view();
+    if ((<Ship>this.actor.system)?.deckPlan) {
+      game.scenes?.get((<Ship>this.actor.system).deckPlan)?.view();
     }
   }
 
   private _onDeckplanUnlink() {
-    if ((<Ship>this.actor.data.data)?.deckPlan) {
-      this.actor.update({"data.deckPlan": ""});;
+    if ((<Ship>this.actor.system)?.deckPlan) {
+      this.actor.update({"system.deckPlan": ""});;
     }
   }
 
@@ -193,12 +193,12 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
     const li = $(event.currentTarget).parents(".item");
     const itemSelected = this.actor.items.get(li.data("itemId"));
     if (itemSelected) {
-      const newHits = (<Component>itemSelected.data.data)?.hits + modifier;
+      const newHits = (<Component>itemSelected.system)?.hits + modifier;
       if (newHits <= game.settings.get('twodsix', 'maxComponentHits') && newHits >= 0) {
-        await itemSelected.update({ "data.hits": newHits });
+        await itemSelected.update({ "system.hits": newHits });
       }
       if (newHits === game.settings.get('twodsix', 'maxComponentHits')) {
-        await itemSelected.update({ "data.status": "destroyed" });
+        await itemSelected.update({ "system.status": "destroyed" });
       }
     }
   }
@@ -208,7 +208,7 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
       const actor = game.actors?.get($(event.target).data("id"));
       event.dataTransfer.setData("text/plain", JSON.stringify({
         "type": "Actor",
-        "data": actor?.data,
+        "data": actor,  //Not Certain if this should be system instead
         "actorId": this.actor.id,
         "id": $(event.target).data("id")
       }));
@@ -225,39 +225,30 @@ export class TwodsixShipSheet extends AbstractTwodsixActorSheet {
       return false;
     }
 
-
     try {
-      const data = getDataFromDropEvent(event);
+      const dropData:any = getDataFromDropEvent(event);
+      const droppedObject:any = await getItemDataFromDropData(dropData);
 
-      if (data.type === "Actor" && (data.data?.type === "traveller" || game.actors?.get(data.id)?.type === "traveller")) {
-        const actorId = data.id;
-        const currentShipPositionId = (<Ship>this.actor.data.data).shipPositionActorIds[actorId];
+      if (droppedObject.type === "traveller") {
+        const actorId = droppedObject._id;
+        const currentShipPositionId = (<Ship>this.actor.system).shipPositionActorIds[actorId];
         if (event.target !== null && $(event.target).parents(".ship-position").length === 1) {
           const shipPositionId = $(event.target).parents(".ship-position").data("id");
-          await this.actor.update({[`data.shipPositionActorIds.${actorId}`]: shipPositionId});
+          await this.actor.update({[`system.shipPositionActorIds.${actorId}`]: shipPositionId});
           this.actor.items.get(shipPositionId)?.sheet?.render();
         } else {
-          await this.actor.update({[`data.shipPositionActorIds.-=${actorId}`]: null});
+          await this.actor.update({[`system.shipPositionActorIds.-=${actorId}`]: null});
         }
         this.actor.items.get(currentShipPositionId)?.sheet?.render();
-      } else if (data.type === "Item" && (data.data?.type === "skills" || game.items?.get(data.id)?.type === "skills") && event.target !== null && $(event.target).parents(".ship-position").length === 1) {
+      } else if ((droppedObject.type === "skills") && event.target !== null && $(event.target).parents(".ship-position").length === 1) {
         const shipPositionId = $(event.target).parents(".ship-position").data("id");
         const shipPosition = <TwodsixItem>this.actor.items.get(shipPositionId);
-
-        let skillData:TwodsixItem|undefined;
-        if (data.id) {
-          skillData = <TwodsixItem><unknown>(game.items?.get(data.id));
-        } else {
-          skillData = <TwodsixItem><unknown>game.actors?.get(data.actorId)?.items.get(data.data._id);
-        }
-        if (skillData) {
-          await TwodsixShipPositionSheet.createActionFromSkill(shipPosition, skillData);
-        }
+        await TwodsixShipPositionSheet.createActionFromSkill(shipPosition, droppedObject);
       } else {
         return super._onDrop(event);
       }
     } catch (err) {
-      // console.warn(err); // uncomment when debugging
+      console.warn(err); // uncomment when debugging
       return false;
     }
   }
