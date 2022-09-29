b84f1c47b9032864eebfe9f16cb6e8def8595916
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-09-29 14:52:14 -0500

    simplify prior commit

--- src/module/entities/TwodsixActor.ts
+++ src/module/entities/TwodsixActor.ts
@@ -167,7 +167,7 @@ export default class TwodsixActor extends Actor {
     }
 
     function calculateComponentCost(anComponent: Component, weightForItem: number, shipActor): void {
-      if (anComponent.subtype !== "fuel" && anComponent.subtype !== "cargo" && anComponent.subtype !== "vehicle") {
+      if (!["fuel", "cargo", "vehicle"].includes(anComponent.subtype)) {
         if (anComponent.subtype === "hull") {
           switch (anComponent.pricingBasis) {
             case "perUnit":
