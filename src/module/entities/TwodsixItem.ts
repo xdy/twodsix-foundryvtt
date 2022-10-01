330e9078164333fc40a2f651cb094814c9a09071
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-10-01 09:03:19 -0500

    add "dd" nomenclature to damage rolls

--- src/module/entities/TwodsixItem.ts
+++ src/module/entities/TwodsixItem.ts
@@ -245,6 +245,7 @@ export default class TwodsixItem extends Item {
 
       let damage = <Roll>{};
       let apValue = 0;
+      rollFormula = rollFormula.replace(/dd/ig, "d6*10");
       if (Roll.validate(rollFormula)) {
         damage = new Roll(rollFormula, this.actor?.system);
         await damage.evaluate({async: true}); // async: true will be default in foundry 0.10
