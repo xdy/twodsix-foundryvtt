1b788d47320f0b669687f77febb065eb4cf9310d
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2022-10-01 09:09:17 -0500

    add comment

--- src/module/entities/TwodsixItem.ts
+++ src/module/entities/TwodsixItem.ts
@@ -245,7 +245,7 @@ export default class TwodsixItem extends Item {
 
       let damage = <Roll>{};
       let apValue = 0;
-      rollFormula = rollFormula.replace(/dd/ig, "d6*10");
+      rollFormula = rollFormula.replace(/dd/ig, "d6*10"); //Parse for a destructive damage roll DD = d6*10
       if (Roll.validate(rollFormula)) {
         damage = new Roll(rollFormula, this.actor?.system);
         await damage.evaluate({async: true}); // async: true will be default in foundry 0.10
