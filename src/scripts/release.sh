bad3e91e6c2c6ced97340a132d077abc2563cfd8
Jonas Karlsson <xdy@users.noreply.github.com>
2022-10-25 16:15:27 +0200

    fix: Attempt at re-release after fixing release script.
    fix: trailing commas and dropped skills(#1150)
    
    This release fixes a number of small issues and also does some behind the scenes improvements to better leverage intrinsic FVTT capabilities in v10:
    * Trailing commas on npc and animal sheet are removed
    * Dropping duplicate skills now increases level by 1 rather than doing nothing (also sets level to at least the minimum display value based on the hide untrained setting). 
    * Skill names are sorted in drop-down lists
    * Cleans-up errors in English localization file
    * Improves Spanish localization (courtesy of @ForjaSalvaje )
    
    Because of some of the backend changes, there may be a few bugs introduced.  Please let us know!

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -5,10 +5,10 @@ VERSION_MAIN_REPLACE="\1download/v$1\3"
 #For latest download
 LATEST_MAIN_REPLACE="\1latest/download\3"
 
-sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" system.json &&
-sed -i -r s"~${MANIFEST_SEARCH_PATTERN}~${LATEST_MAIN_REPLACE}~" system.json &&
-sed -i -r s"~${DOWNLOAD_SEARCH_PATTERN}~${VERSION_MAIN_REPLACE}~" system.json &&
-cp system.json dist &&
+sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" static/system.json &&
+sed -i -r s"~${MANIFEST_SEARCH_PATTERN}~${LATEST_MAIN_REPLACE}~" static/system.json &&
+sed -i -r s"~${DOWNLOAD_SEARCH_PATTERN}~${VERSION_MAIN_REPLACE}~" static/system.json &&
+cp static/system.json dist &&
 sed -i -e 's|\(.*"version"\): "\(.*\)",.*|\1: '"\"$1\",|" package.json &&
 npm install &&
 cd dist || exit &&
