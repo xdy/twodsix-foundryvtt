b824051b3b85220d05ccff92a689f27db18e168c
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-02-17 14:19:39 -0600

    fix: make chat buttons compatible with ship rolls

--- src/module/utils/TwodsixDiceRoll.ts
+++ src/module/utils/TwodsixDiceRoll.ts
@@ -292,9 +292,9 @@ export class TwodsixDiceRoll {
 
     //Add buttons
     flavorText += `<section class="card-buttons"><button data-action="expand" data-tooltip="${game.i18n.localize("TWODSIX.Rolls.ToggleDetails")}"><i class="fa-solid fa-circle-question"></i></button>`;
-    if (this.isSuccess() && !game.settings.get("twodsix", "automateDamageRollOnHit") && this.item?.type === "weapon") {
+    if (this.isSuccess() && !game.settings.get("twodsix", "automateDamageRollOnHit") && (this.item?.type === "weapon" || (this.item?.type === "component" && this.item?.system?.subtype === "armament"))) {
       flavorText += `<button data-action="damage" data-tooltip="${game.i18n.localize("TWODSIX.Rolls.RollDamage")}"><i class="fa-solid fa-person-burst"></i></button>`;
-    } else if (this.rollSettings.skillRoll && this.item?.type !== "weapon") {
+    } else if (this.rollSettings.skillRoll && this.item?.type !== "weapon" && !(this.item?.type === "component" && this.item?.system?.subtype === "armament")) {
       flavorText += `<button data-action="chain" data-tooltip="${game.i18n.localize("TWODSIX.Rolls.RollChain")}"><i class="fa-solid fa-link"></i></button>`;
       flavorText += `<button data-action="opposed" data-tooltip="${game.i18n.localize("TWODSIX.Rolls.RollOpposed")}"><i class="fa-solid fa-down-left-and-up-right-to-center"></i></button>`;
     }
