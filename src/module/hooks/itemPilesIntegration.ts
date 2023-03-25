6219a5481cb2ac8609393ee07c0a4654528fb9b6
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-03-25 12:02:30 -0500

    turn off type checking for hook

--- src/module/hooks/itemPilesIntegration.ts
+++ src/module/hooks/itemPilesIntegration.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
 Hooks.once("item-piles-ready", () => {
   /// Called once Item Piles is ready to be used
   //Set Item Piles filtered items
