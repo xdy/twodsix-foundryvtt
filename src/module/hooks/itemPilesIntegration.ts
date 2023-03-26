dfacac8e0103f32f9f8192f8cf4e2123d0c8b849
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-03-26 13:26:20 -0500

    fix: style and localization fix for Item Piles (#1280)
    This small update moves Item Piles full initialization into Twodsix as well as fixes some style issues.  Also, updates to Spanish translation added thanks to @ForjaSalvaje.

--- src/module/hooks/itemPilesIntegration.ts
+++ src/module/hooks/itemPilesIntegration.ts
@@ -1,12 +1,51 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
-Hooks.once("item-piles-ready", () => {
+Hooks.once("item-piles-ready", async function() {
   /// Called once Item Piles is ready to be used
-  //Set Item Piles filtered items
-  game.itempiles.API.setItemFilters([
-    {path: 'type', filters: 'skills'},
-    {path: 'type', filters: 'trait'},
-    {path: 'type', filters: 'spell'},
-    {path: 'name', filters: 'Unarmed'}
-  ]);
+  game.itempiles.API.addSystemIntegration({
+    "VERSION": "1.0.3",
+
+    // The actor class type is the type of actor that will be used for the default item pile actor that is created on first item drop.
+    "ACTOR_CLASS_TYPE": "traveller",
+
+    // The item quantity attribute is the path to the attribute on items that denote how many of that item that exists
+    "ITEM_QUANTITY_ATTRIBUTE": "system.quantity",
+
+    // The item price attribute is the path to the attribute on each item that determine how much it costs
+    "ITEM_PRICE_ATTRIBUTE": "system.price",
+
+    // Item filters actively remove items from the item pile inventory UI that users cannot loot, such as spells, feats, and classes
+    "ITEM_FILTERS": [
+      {
+        "path": "type",
+        "filters": "skills,trait,spell"
+      },
+      {
+        "path": "name",
+        "filters": game.i18n.localize("TWODSIX.Items.Weapon.Unarmed")
+      }
+    ],
+
+    // Item similarities determines how item piles detect similarities and differences in the system
+    "ITEM_SIMILARITIES": ["name", "type", "techLevel"],
+
+    // Currencies in item piles is a versatile system that can accept actor attributes (a number field on the actor's sheet) or items (actual items in their inventory)
+    // In the case of attributes, the path is relative to the "actor.system"
+    // In the case of items, it is recommended you export the item with `.toObject()` and strip out any module data
+    "CURRENCIES": [
+      {
+        type: "attribute",
+        name: "Credits",
+        img: "systems/twodsix/assets/icons/id-card.svg",
+        abbreviation: "Cr {#}",
+        data: {
+          path: "system.finances.cash"
+        },
+        primary: true,
+        exchangeRate: 1
+      }
+    ],
+
+    "CURRENCY_DECIMAL_DIGITS": 0.01
+  });
 });
