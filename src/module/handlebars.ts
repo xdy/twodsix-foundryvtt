e9468c4b484ba2fb2738b810d3d39092db2cdfcc
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-04-22 10:37:08 -0500

    fix deep scan issue

--- src/module/handlebars.ts
+++ src/module/handlebars.ts
@@ -261,7 +261,7 @@ export default function registerHandlebarsHelpers(): void {
       return 0;
     } else {
       let bandwidth = 0;
-      if (item?.system.attachmentData) {
+      if (item.system.attachmentData) {
         for (const attch of item.system.attachmentData) {
           if (attch.system.subtype === "software" && attch.system.softwareActive) {
             bandwidth += attch.system.bandwidth;
