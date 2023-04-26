1e660128bc34c2e2836ae9284b29feebc4267705
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-04-26 13:56:10 -0500

    refactor chat cards

--- src/module/hooks/chatCard.ts
+++ src/module/hooks/chatCard.ts
@@ -1,17 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
 import TwodsixItem from "../entities/TwodsixItem";
-import ItemTemplate from "../utils/ItemTemplate";
+//import ItemTemplate from "../utils/ItemTemplate";
 import { getControlledTraveller } from "../sheets/TwodsixVehicleSheet";
 import TwodsixActor from "../entities/TwodsixActor";
 import { TwodsixDiceRoll } from "../utils/TwodsixDiceRoll";
 import { TwodsixRollSettings } from "../utils/TwodsixRollSettings";
 
-Hooks.on("renderChatLog", (app, html, data) => {
+Hooks.on("renderChatLog", (_app, html, _data) => {
   html.on("click", ".card-buttons button", onChatCardAction);
   html.on("click", ".item-name", onChatCardToggleContent);
 });
-Hooks.on("renderChatPopout", (app, html, data) => {
+Hooks.on("renderChatPopout", (_app, html, _data) => {
   html.on("click", ".card-buttons button", onChatCardAction);
   html.on("click", ".item-name", onChatCardToggleContent);
 });
@@ -49,81 +49,55 @@ async function onChatCardAction(event: Event): Promise<any> {
   }
   const action = button.dataset.action;
 
-  // Recover the actor for the chat card
-  const actor = await getChatCardActor(message);
-  if ( !actor ) {
+  if (action === "expand") {
+    onExpandClick(event);
     return;
-  }
-
-  // Validate permission to proceed with the roll
-  const isTargetted = ["chain", "opposed", "expand"].includes(action);
-  if ( !( isTargetted || game.user.isGM || actor.isOwner ) ) {
+  } else if (action === "abilityCheck") {
     return;
-  }
-  // Get the Item from stored flag data
-  const storedData = message.getFlag("twodsix", "itemUUID");
-  const item:TwodsixItem = storedData ? await fromUuid(storedData) : {};
-  if ( !item ) {
-    const err = game.i18n.format("DND5E.ActionWarningNoItem", {item: card.dataset.itemId, name: actor.name});
-    return ui.notifications.error(err);
+  } else {
+    // Recover the actor for the chat card
+    const actor = await getChatCardActor(message);
+    if ( !actor ) {
+      return;
+    }
+
+    // Validate permission to proceed with the roll
+    const isTargettedAction = ["chain", "opposed"].includes(action);
+    if ( !( isTargettedAction || game.user.isGM || actor.isOwner ) ) {
+      return;
+    }
+    // Get the Item from stored flag data
+    const storedData = message.getFlag("twodsix", "itemUUID");
+    const item:TwodsixItem = storedData ? await fromUuid(storedData) : {};
+    if ( !item ) {
+      const err = game.i18n.format("DND5E.ActionWarningNoItem", {item: card.dataset.itemId, name: actor.name});
+      return ui.notifications.error(err);
+    }
+
+    // Handle different actions
+    //let targets;
+    const useInvertedShiftClick:boolean = (<boolean>game.settings.get('twodsix', 'invertSkillRollShiftClick'));
+    const showFormulaDialog = useInvertedShiftClick ? event["shiftKey"] : !event["shiftKey"];
+    const bonusDamage:string = message.getFlag("twodsix", "bonusDamage");
+    const effect = message.getFlag("twodsix", "effect") ?? 0;
+    const totalBonusDamage = (bonusDamage !== "0" && bonusDamage !== "") ? `${effect} + ${bonusDamage}` : `${effect}`;
+    switch ( action ) {
+      case "damage":
+        await item.rollDamage((<DICE_ROLL_MODES>game.settings.get('core', 'rollMode')), totalBonusDamage, true, showFormulaDialog);
+        break;
+      case "opposed":
+        //opposed roll
+        makeSecondaryRoll(message, "opposed", showFormulaDialog);
+        break;
+      case "chain":
+        //chain roll
+        makeSecondaryRoll(message, "chain", showFormulaDialog);
+        break;
+      default:
+        break;
+    }
   }
 
-  // Handle different actions
-  //let targets;
-  const useInvertedShiftClick:boolean = (<boolean>game.settings.get('twodsix', 'invertSkillRollShiftClick'));
-  const showFormulaDialog = useInvertedShiftClick ? event["shiftKey"] : !event["shiftKey"];
-  const bonusDamage:string = message.getFlag("twodsix", "bonusDamage");
-  const effect = message.getFlag("twodsix", "effect") ?? 0;
-  const totalBonusDamage = (bonusDamage !== "0" && bonusDamage !== "") ? `${effect} + ${bonusDamage}` : `${effect}`;
-  switch ( action ) {
-    case "attack":
-      break;
-    case "damage":
-      await item.rollDamage((<DICE_ROLL_MODES>game.settings.get('core', 'rollMode')), totalBonusDamage, true, showFormulaDialog);
-      break;
-    case "versatile":
-      break;
-    case "formula":
-      break;
-    case "save":
-      //targets = getChatCardTargets();
-      //for ( const token of targets ) {
-      //  const speaker = ChatMessage.getSpeaker({scene: canvas.scene, token: token.document});
-      //  await token.actor.rollAbilitySave(button.dataset.ability, { event, speaker });
-      //}
-      break;
-    case "toolCheck":
-      //await item.rollToolCheck({event});
-      break;
-    case "placeTemplate":
-      try {
-        await ItemTemplate.fromItem(item)?.drawPreview();
-      } catch(err) {/*blank*/}
-      break;
-    case "abilityCheck":
-      //targets = getChatCardTargets();
-      //for ( const token of targets ) {
-      //  const speaker = ChatMessage.getSpeaker({scene: canvas.scene, token: token.document});
-      //  await token.actor.rollAbilityTest(button.dataset.ability, { event, speaker });
-      //}
-      break;
-    case "expand":
-      onExpandClick(event);
-      break;
-    case "opposed":
-      //opposed roll
-      makeSecondaryRoll(message, "opposed", showFormulaDialog);
-      break;
-    case "chain":
-      //chain roll
-      makeSecondaryRoll(message, "chain", showFormulaDialog);
-      break;
-    default:
-      break;
-  }
-
-  // Re-enable the button
-  //button.disabled = false;
 }
 
 /* -------------------------------------------- */
@@ -184,23 +158,19 @@ async function getChatCardActor(message:ChatMessage): Actor | null {
   * @private
   */
 function onExpandClick(event: Event) {
-  event.preventDefault();
-
   // Toggle the message flag
-  const roll = event.currentTarget;
-  //message._rollExpanded = !message._rollExpanded;
+  //must be an easier way to get the higher target
+  const roll = event.currentTarget.parentElement.parentElement.parentElement.parentElement;
 
   // Expand or collapse chattips
-  const chattips = roll.querySelectorAll(".dice-chattip");
-  for ( const tip of chattips ) {
-    if ( $(tip).css("display") !== "none" ) {
-      //$(tip).slideUp(200);
-      $(tip).css("display", "none");
-    } else {
-      //$(tip).slideDown(200);
-      $(tip).css("display", "contents");
-    }
-    //tip.classList.toggle("expanded", message._rollExpanded);
+  const tip = roll.querySelector(".dice-chattip");
+  //for ( const tip of chattips ) {
+  if ( $(tip).css("display") !== "none" ) {
+    //$(tip).slideUp(200);
+    $(tip).css("display", "none");
+  } else {
+    //$(tip).slideDown(200);
+    $(tip).css("display", "contents");
   }
 }
 /**
