f1f7e497d5e800d1ae67a902ceed72d15aef08a9
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-06-15 15:23:18 -0500

    fix: add scaled damage

--- src/module/hooks/addChatContext.ts
+++ src/module/hooks/addChatContext.ts
@@ -25,6 +25,18 @@ function addChatMessageContextOptions(html, options) {
       callback: li => applyChatCardDamage(li, 1)
     },
     {
+      name: game.i18n.localize("TWODSIX.Chat.Roll.ApplyDestructiveDamage"),
+      icon: '<i class="fas fa-user-injured"></i>',
+      condition: canApply,
+      callback: li => applyChatCardDamage(li, 10)
+    },
+    {
+      name: game.i18n.localize("TWODSIX.Chat.Roll.ApplyReducedDamage"),
+      icon: '<i class="fas fa-user-shield"></i>',
+      condition: canApply,
+      callback: li => applyChatCardDamage(li, 0.1)
+    },
+    {
       name: game.i18n.localize("TWODSIX.Chat.Roll.ApplyHealing"),
       icon: '<i class="fas fa-user-plus"></i>',
       condition: canApply,
@@ -44,13 +56,14 @@ function addChatMessageContextOptions(html, options) {
 function applyChatCardDamage(li, multiplier) {
   const message = game.messages.get(li.data("messageId"));
   const transfer = message.flags.transfer ? JSON.parse(message.flags.transfer) : undefined;
-  const effect = transfer?.payload.damageValue ?? message.flags.twodsix?.effect;
+  const effect = transfer?.payload.damageValue ?? message.flags.twodsix?.effect ?? message.rolls[0].total;
   if (effect > 0) {
     return Promise.all(canvas.tokens.controlled.map(t => {
       if (["traveller", "robot", "animal"].includes(t.actor.type)) {
-        if (multiplier > 0) {
-          t.actor.damageActor(effect, transfer?.payload.armorPiercingValue ?? 0, transfer?.payload.damageType ?? "", true);
-        } else {
+        const damage = Math.floor(effect * multiplier);
+        if (damage > 0) {
+          t.actor.damageActor(Math.floor(effect * multiplier), transfer?.payload.armorPiercingValue ?? 0, transfer?.payload.damageType ?? "", true);
+        } else if (multiplier < 0) {
           t.actor.healActor(effect);
         }
       }
