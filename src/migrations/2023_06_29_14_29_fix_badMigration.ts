9ef72fc3ec18d2ddb9626517ca24bbacac14ca09
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-06-29 14:35:49 -0500

    fix: bad radiationProtection value

---
+++ src/migrations/2023_06_29_14_29_fix_badMigration.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
+
+import { applyToAllActors} from "../module/utils/migration-utils";
+
+async function refactorLinkedEffects (actor: TwodsixActor): Promise<void> {
+  if (["traveller", "animal", "robot"].includes(actor.type)) {
+    if(!isNaN(actor.system.radiationProteciton)) {
+      actor.update({"system.radiationProtection": {value: 0}});
+      console.log(`Updating ${actor.name}'s radiation protection`);
+    }
+  }
+}
+
+export async function migrate(): Promise<void> {
+  await applyToAllActors(refactorLinkedEffects);
+  return Promise.resolve();
+}
