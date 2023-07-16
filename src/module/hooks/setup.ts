f26f5c24fa02f5a6f20fa8ad32415a9f10dd21d5
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-07-16 12:08:56 -0500

    feat: add new status icons

--- src/module/hooks/setup.ts
+++ src/module/hooks/setup.ts
@@ -5,7 +5,14 @@ import {TwodsixSystem} from "../TwodsixSystem";
 Hooks.once('setup', async function () {
   // Do anything after initialization but before ready
 
-  //Configure conditions
+  //Configure TWODSIX custom conditions
+  CONFIG.statusEffects.push(
+    {id: 'encumbered', label: 'EFFECT.StatusEncumbered', icon: "systems/twodsix/assets/icons/weight.svg"},
+    {id: 'aiming', label: 'EFFECT.StatusAiming', icon: "systems/twodsix/assets/icons/aiming.svg"},
+    {id: 'fatigued', label: 'EFFECT.StatusFatigued', icon: "systems/twodsix/assets/icons/tired-eye.svg"},
+    {id: 'cover', label: 'EFFECT.StatusInCover', icon: "systems/twodsix/assets/icons/defensive-wall.svg"},
+    {id: 'thrust', label: 'EFFECT.StatusThrust', icon: "systems/twodsix/assets/icons/thrust.svg"}
+  );
   CONFIG.statusEffects.push({id: 'encumbered', label: 'EFFECT.StatusEncumbered', icon: "systems/twodsix/assets/icons/weight.svg"});
   const woundedSE = CONFIG.statusEffects.find(se => se.id === 'bleeding');
   if(woundedSE) {
