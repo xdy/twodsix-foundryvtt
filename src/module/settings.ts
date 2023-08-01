e35f20bbed2ea3402adef8531a1ab3d46b8af0f1
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-08-01 13:36:20 -0500

    feat: add setting to override auto damage roll for clients (#1426)
    This much needed update adds a new setting so that the GM's can choose to override the auto damage roll setting for players.  The auto damage roll setting is, currently, the only setting that has a 'client' rather than a 'world' scope.  Thus, each player can configure as they prefer.  With the new setting, "Override automatic damage rolls to GM Setting" - checking the box (to true) forces the auto damage setting to the state that the GM has chosen and removes the setting option for all players.  Note that a reload is required for all clients when changing this setting. This new setting might be useful where you'd like consistency in how the players roll damage or when there are users unfamiliar with FVTT.
    
    This update also adds:
    * Some style fixes for Monks Active Title Triggers as noted by @Vertigo (through experimental/debug setting to correct CSS styles).
    * A new macro to roll Traveller characteristics courtesy of @JoshB.

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
 import RulsetSettings from "./settings/RulsetSettings";
 import ItemSettings from "./settings/ItemSettings";
 import DisplaySettings from "./settings/DisplaySettings";
@@ -27,8 +29,19 @@ export const registerSettings = function ():void {
     return 0;
   });
   stringChoiceSetting('ruleset', TWODSIX.RULESETS["CE"].key, false, Object.fromEntries(rulesetOptions), true);
+  //need a custom setting to use requiresReload
+  game.settings.register("twodsix", "overrideDamageRoll", {
+    name: game.i18n.localize("TWODSIX.Settings.overrideDamageRoll.name"),
+    hint: game.i18n.localize("TWODSIX.Settings.overrideDamageRoll.hint"),
+    scope: "world",
+    config: true,
+    requiresReload: true,
+    type: Boolean,
+    default: false,
+    onChange: _overrideDamageRollSetting
+  });
 
-  booleanSetting('automateDamageRollOnHit', true, true, 'client');
+  booleanSetting('automateDamageRollOnHit', true, true, game.settings.get('twodsix', 'overrideDamageRoll')? 'world' : 'client');
   booleanSetting('hideUntrainedSkills', false, true, "world", _onHideUntrainedSkillsChange);
   booleanSetting('invertSkillRollShiftClick', false, true);
   booleanSetting('transferDroppedItems', false, true);
@@ -40,6 +53,14 @@ export const registerSettings = function ():void {
       TwodsixActor.resetUntrainedSkill();
     } else {
       TwodsixActor.setUntrainedSkillForItems();
+    }
+  }
+
+  function  _overrideDamageRollSetting(setting:boolean) {
+    const currentValue = game.settings.get('twodsix', 'automateDamageRollOnHit');
+    if (currentValue !== undefined) {
+      const setScope = setting ? 'world' : 'client';
+      game.settings.set('twodsix', 'automateDamageRollOnHit', currentValue, {scope: setScope, default: currentValue, config: true, type: Boolean});
     }
   }
 };
