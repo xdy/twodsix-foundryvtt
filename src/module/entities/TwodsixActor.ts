f249ce3c80a1583efbe25f782fd90600bf317dfa
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-09-25 16:48:36 -0500

    fix: make a doShipAction method to work with TAH

--- src/module/entities/TwodsixActor.ts
+++ src/module/entities/TwodsixActor.ts
@@ -16,6 +16,7 @@ import {Characteristic, Component, Gear, Ship, Skills, Traveller} from "../../ty
 import { getCharShortName } from "../utils/utils";
 import { applyToAllActors } from "../utils/migration-utils";
 import { applyEncumberedEffect } from "../hooks/showStatusIcons";
+import { TwodsixShipActions } from "../utils/TwodsixShipActions";
 
 export default class TwodsixActor extends Actor {
   /**
@@ -1043,6 +1044,10 @@ export default class TwodsixActor extends Actor {
       }
     }
     return returnObject;
+  }
+
+  public doShipAction(action, extra):void {
+    TwodsixShipActions.availableMethods[action.type].action(action.command, extra);
   }
 
 }
