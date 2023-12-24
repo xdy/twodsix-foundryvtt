2841915ab47c86eb46a46b845290ab91c447a230
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-12-24 09:44:06 -0600

    fix: add roll valid check to CUSTOM AE mode

--- src/module/hooks/applyActiveEffects.ts
+++ src/module/hooks/applyActiveEffects.ts
@@ -4,24 +4,24 @@
 import TwodsixActor from "../entities/TwodsixActor";
 //This hook applies CUSTOM active effects values as a formula that is evaluated and not a static
 Hooks.on('applyActiveEffect', (actor:TwodsixActor, change:any, current: any, delta: any) => {
-  //const r = new Roll(delta, actor);
-  //r.evaluate({async: false});
-  const r = Roll.safeEval(Roll.replaceFormulaData(delta, actor));
-  const ct = foundry.utils.getType(current);
   let update = 0;
-  switch ( ct ) {
-    case "string": {
-      if (Number.isInteger(Number.parseFloat(current))) {
-        update = (parseInt(current) + parseInt(r));
-      } else {
-        update = Number.parseFloat(current) + r;
+  if (Roll.validate(delta)) {
+    const r = Roll.safeEval(Roll.replaceFormulaData(delta, actor));
+    const ct = foundry.utils.getType(current);
+    switch ( ct ) {
+      case "string": {
+        if (Number.isInteger(Number.parseFloat(current))) {
+          update = (parseInt(current) + parseInt(r));
+        } else {
+          update = Number.parseFloat(current) + r;
+        }
+        break;
       }
-      break;
-    }
-    case "number":
-    {
-      update = current + r;
-      break;
+      case "number":
+      {
+        update = current + r;
+        break;
+      }
     }
   }
   foundry.utils.setProperty(actor, change.key, update);
