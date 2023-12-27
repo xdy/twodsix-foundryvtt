b5965d83ba6cd2621bc0dae5277c0374a81a7ed6
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2023-12-27 13:22:05 -0600

    feat: add melee range modifier

--- src/module/settings/RulsetSettings.ts
+++ src/module/settings/RulsetSettings.ts
@@ -103,6 +103,7 @@ export default class RulesetSettings extends AdvancedSettings {
     settings.roll.push(stringChoiceSetting('useDegreesOfSuccess', "none", true, TWODSIX.SuccessTypes));
     settings.roll.push(booleanSetting("overrideSuccessWithNaturalCrit", false));
     settings.general.push(stringChoiceSetting('rangeModifierType', "none", true, TWODSIX.RANGE_MODIFIERS_TYPES));
+    settings.general.push(numberSetting("meleeRange", 2, false, "world"));
     settings.roll.push(largeStringSetting('targetDMList', "", false, "world", generateTargetDMObject));
     return settings;
   }
