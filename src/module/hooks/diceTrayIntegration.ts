adedb6bc12cbaf7274d2bd8414ee2b1246bc18f2
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2024-01-23 08:10:47 -0600

    fix: update dice-tray button integration (#1538)
    This update moves dice tray module integration to the current API.  Thanks to @mclemente for the code.

--- src/module/hooks/diceTrayIntegration.ts
+++ src/module/hooks/diceTrayIntegration.ts
@@ -4,77 +4,70 @@ import { getKeyByValue } from "../utils/sheetUtils";
 import { simplifySkillName } from "../utils/utils";
 import {TWODSIX} from "../config";
 
-Hooks.on('dcCalcWhitelist', (whitelist, actor) => {
-  // Add whitelist support for the calculator.
-  whitelist.twodsix = {
-    attributes: [],
-    abilities: [],
-    custom: {
-      attributes: {},
-      abilities: {},
-      custom: {}
-    }
-  };
+Hooks.on('dice-calculator.calculator', (calculators, Template) => {
+  class twodsixDiceCalculator extends Template {
+    actorSpecificButtons(actor) {
+      const abilities = [];
+      const attributes = [];
+      const customButtons = [];
 
-  if (game.settings.get('twodsix', 'lifebloodInsteadOfCharacteristics')) {
-    whitelist.twodsix.custom.attributes = {
-      endurance: {
-        label: "endurance",
-        name: game.i18n.localize("TWODSIX.Actor.Characteristics.END"),
-        formula: actor.system.characteristics.endurance.current !== undefined ? `(@characteristics.endurance.current)[${game.i18n.localize("TWODSIX.Actor.Characteristics.END")}]` : ``
-      },
-      lifeblood: {
-        label: "lifeblood",
-        name: game.i18n.localize("TWODSIX.Actor.Characteristics.LFB"),
-        formula: actor.system.characteristics.strength.current !== undefined ? `(@characteristics.strength.current)[${game.i18n.localize("TWODSIX.Actor.Characteristics.LFB")}]` : ``
+      if (actor) {
+        if (game.settings.get('twodsix', 'lifebloodInsteadOfCharacteristics')) {
+          attributes.push(...[
+            {
+              label: 'endurance',
+              name: game.i18n.localize('TWODSIX.Actor.Characteristics.END'),
+              formula: actor.system.characteristics.endurance.current !== undefined ? `(@characteristics.endurance.current)[${game.i18n.localize('TWODSIX.Actor.Characteristics.END')}]` : ''
+            },
+            {
+              label: 'lifeblood',
+              name: game.i18n.localize('TWODSIX.Actor.Characteristics.LFB'),
+              formula: actor.system.characteristics.strength.current !== undefined ? `(@characteristics.strength.current)[${game.i18n.localize('TWODSIX.Actor.Characteristics.LFB')}]` : ''
+            }]);
+          if (game.settings.get('twodsix', 'showContaminationBelowLifeblood')) {
+            attributes.push({
+              label: 'contamination',
+              name: game.i18n.localize('TWODSIX.Actor.Characteristics.CTM'),
+              formula: actor.system.characteristics.psionicStrength.current !== undefined ? `(@characteristics.psionicStrength.current)[${game.i18n.localize('TWODSIX.Actor.Characteristics.CTM')}]` : ''
+            });
+          }
+        } else {
+          const filter = ['stamina', 'lifeblood'];
+          switch (game.settings.get('twodsix', 'showAlternativeCharacteristics')) {
+            case 'core':
+              filter.push(...['psionicStrength', 'alternative1', 'alternative2', 'alternative3']);
+              break;
+            case 'base':
+              filter.push(...['alternative1', 'alternative2', 'alternative3']);
+              break;
+            case 'alternate':
+              filter.push(...['psionicStrength', 'alternative3']);
+              break;
+            case 'all':
+              break;
+          }
+          for (const char in actor.system.characteristics) {
+            if (!filter.includes(char)) {
+              attributes.push({
+                label: actor.system.characteristics[char].key,
+                name: actor.system.characteristics[char].displayShortLabel,
+                formula: actor.system.characteristics[char].mod !== undefined ? `(@characteristics.${char}.mod)[${actor.system.characteristics[char].displayShortLabel}]` : ''
+              });
+            }
+          }
+        }
+        for (const skill of actor.itemTypes.skills) {
+          const simpleSkillName = simplifySkillName(skill.name);
+          const fullCharLabel = skill.system.characteristic !== 'NONE' ? getKeyByValue(TWODSIX.CHARACTERISTICS, skill.system.characteristic) : '';
+          customButtons.push({
+            label: simpleSkillName,
+            name: skill.name,
+            formula: `(@skills.${simpleSkillName}${fullCharLabel !== '' ? ` + @characteristics.${fullCharLabel}.mod` : ''})[${skill.name}]`
+          });
+        }
       }
-    };
-    if (game.settings.get('twodsix', 'showContaminationBelowLifeblood')) {
-      whitelist.twodsix.custom.attributes.contamination = {
-        label: "contamination",
-        name: game.i18n.localize("TWODSIX.Actor.Characteristics.CTM"),
-        formula: actor.system.characteristics.psionicStrength.current !== undefined ? `(@characteristics.psionicStrength.current)[${game.i18n.localize("TWODSIX.Actor.Characteristics.CTM")}]` : ``
-      };
-    }
-  } else {
-    for (const char in actor.system.characteristics) {
-      if (!["stamina", "lifeblood"].includes(char)) {
-        whitelist.twodsix.custom.attributes[char] = {
-          label: actor.system.characteristics[char].key,
-          name: actor.system.characteristics[char].displayShortLabel,
-          formula: actor.system.characteristics[char].mod !== undefined ? `(@characteristics.${char}.mod)[${actor.system.characteristics[char].displayShortLabel}]` : ``
-        };
-      }
-    }
-    switch (game.settings.get('twodsix', 'showAlternativeCharacteristics')) {
-      case 'core':
-        delete whitelist.twodsix.custom.attributes.psionicStrength;
-        delete whitelist.twodsix.custom.attributes.alternative1;
-        delete whitelist.twodsix.custom.attributes.alternative2;
-        delete whitelist.twodsix.custom.attributes.alternative3;
-        break;
-      case 'base':
-        delete whitelist.twodsix.custom.attributes.alternative1;
-        delete whitelist.twodsix.custom.attributes.alternative2;
-        delete whitelist.twodsix.custom.attributes.alternative3;
-        break;
-      case 'alternate':
-        delete whitelist.twodsix.custom.attributes.psionicStrength;
-        delete whitelist.twodsix.custom.attributes.alternative3;
-        break;
-      case 'all':
-        break;
+      return { abilities, attributes, customButtons };
     }
   }
-
-  for (const skill of actor.itemTypes.skills) {
-    const simpleSkillName = simplifySkillName(skill.name);
-    const fullCharLabel = skill.system.characteristic !== "NONE" ? getKeyByValue(TWODSIX.CHARACTERISTICS, skill.system.characteristic) : "";
-    const formula = `(@skills.${simpleSkillName}` + (fullCharLabel !== "" ? ` + @characteristics.${fullCharLabel}.mod` : ``) + `)[${skill.name}]`;
-    whitelist.twodsix.custom.abilities[simpleSkillName] = {
-      label: simpleSkillName,
-      name: `${skill.name}`,
-      formula: formula
-    };
-  }
+  calculators.twodsix = twodsixDiceCalculator;
 });
