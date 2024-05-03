c37d8efcbbf2e05a8cb7d951151a78ad444e195b
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2024-05-03 18:33:21 -0500

    Update preUpdateActor.ts

--- src/module/hooks/preUpdateActor.ts
+++ src/module/hooks/preUpdateActor.ts
@@ -7,7 +7,7 @@ import { updateHits } from "./updateHits";
 
 Hooks.on('preUpdateActor', (actor:TwodsixActor, update:Record<string, any>) => {
   if (update?.system) {
-    const systemDiff = foundry.utils.diffObject(actor.system._source, update.system); //v12 stopped passing difffernetial
+    const systemDiff = foundry.utils.diffObject(actor.system._source, update.system); //v12 stopped passing diffferential
     updateHits(actor, update, systemDiff);
     updateFinances(actor, update, systemDiff);
   }
