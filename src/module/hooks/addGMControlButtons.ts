c5e542f6d61790aaccf3c579ccadc440bc70aab8
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2024-05-27 10:41:49 -0500

    fix: request roll

--- src/module/hooks/addGMControlButtons.ts
+++ src/module/hooks/addGMControlButtons.ts
@@ -23,8 +23,8 @@ Hooks.on("getSceneControlButtons", (controls) => {
 });
 
 async function requestRoll(): Promise<void> {
-  const tokenData = await getSelectedTokenData();
-  const skillsList = await getAllSkills();
+  const tokenData = getSelectedTokenData();
+  const skillsList = getAllSkills();
   if (Object.keys(tokenData).length > 0) {
     const selections = await throwDialog(skillsList, tokenData);
     if (selections.shouldRoll) {
@@ -69,11 +69,11 @@ function getUserActorList (selections:any, tokenData:any): any {
   return returnData;
 }
 
-async function getAllSkills(): Promise<object> {
+function getAllSkills(): Promise<object> {
   const skillList = {};
-  let selectedActors = await canvas.tokens.controlled.map((t) => t.actor);
+  let selectedActors = canvas.tokens.controlled.map((t) => t.actor);
   if (selectedActors.length === 0) {
-    selectedActors = await game.users.filter(user => !user.isGM && user.active).map((u) => u.character);
+    selectedActors = game.users.filter(user => !user.isGM && user.active).map((u) => u.character);
   }
   for (const actor of selectedActors) {
     for (const skill of actor.itemTypes.skills) {
@@ -146,58 +146,36 @@ async function throwDialog(skillsList:string[], tokenData:any):Promise<any> {
   });
 }
 
-async function getSelectedTokenData(): any {
+function getSelectedTokenData(): any {
   const returnValue = {};
   const validTokens = canvas.tokens.controlled.filter((t) => ["traveller", "animal", "robot"].includes(t.actor.type));
   for (const token of validTokens) {
     returnValue[token.id] = {
-      userId: await getControllingUser(token),
+      userId: getControllingUser(token),
       token: token,
     };
   }
   return returnValue;
 }
 
-async function getControllingUser(token:Token): string {
+function getControllingUser(token:Token): string {
   let userId = "";
-  const owningUsers = await game.users.filter((user) => !user.isGM && user.active && (token.actor.ownership[user.id] === CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER || (token.actor.ownership.default === CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER  && !(user.id in token.actor.ownership))));
+  const owningUsers = game.users.filter((user) => !user.isGM && user.active && (token.actor.ownership[user.id] === CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER || (token.actor.ownership.default === CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER  && !(user.id in token.actor.ownership))));
   if (owningUsers.length > 1) {
-    const characterUser = await owningUsers.find((user) => user.character.id === token.actor.id);
+    const characterUser = owningUsers.find((user) => user.character.id === token.actor.id);
     if (characterUser) {
       userId = characterUser.id;
     } else {
-      const randomSelection =  (await new Roll("1d@length - 1", {length: owningUsers.length}).evaluate()).total;
+      const randomSelection = Math.floor(Math.random() * owningUsers.length);
       userId = owningUsers[randomSelection].id;
     }
   } else if (owningUsers.length === 1) {
     userId = owningUsers[0].id;
   } else {
-    userId = await game.users.find((user) => user.isGM && user.active && token.actor.ownership[user.id] === CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER).id;
+    userId = game.users.find((user) => user.isGM && user.active && token.actor.ownership[user.id] === CONST.DOCUMENT_OWNERSHIP_LEVELS.OWNER)?.id;
+    if (!userId) {
+      userId = game.users.find((user) => user.isGM && user.active)?.id;
+    }
   }
   return userId;
 }
-/*const newControl: SceneControl =
-    {
-      activeTool: "select",
-      name: "gmTools",
-      title: "TWODSIX.Chat.Roll.GMUtils",
-      icon: "fa-solid fa-wand-magic",
-      button: true,
-      visible: game.user.isGM,
-      layer: "tokens",
-      tools: [
-        {
-          name: "requestRoll",
-          title: "TWODSIX.Chat.Roll.RequestRoll",
-          icon: "fa-solid fa-dice",
-          button: true,
-          visible: game.user.isGM,
-          //layer: "gm-utils",
-          onClick: async () => {
-            console.log("Made it to Click!");
-            requestRoll();
-          }
-        }
-      ]
-    };
-    controls.push(newControl);*/
