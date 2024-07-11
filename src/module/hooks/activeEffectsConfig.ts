a874d130197ee20eda11e106df854b5061889118
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2024-07-11 16:09:07 -0500

    fix: apply effect override fails
    move back to hook :(

--- src/module/hooks/activeEffectsConfig.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-Hooks.on(`renderActiveEffectConfig`, (app, _html, _data) => {
-  app.setPosition({width: 700});
-});
