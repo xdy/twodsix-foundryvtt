c74e9f146b734eb719bbdba0d864f11e7d8e7cf4
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2024-09-03 15:26:10 -0500

    fix: add sort of positions

--- src/module/sheets/TwodsixBattleSheet.ts
+++ src/module/sheets/TwodsixBattleSheet.ts
@@ -2,6 +2,7 @@
 // @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
 
 import { TwodsixShipSheet } from "./TwodsixShipSheet";
+//import { TWODSIX } from "../config";
 
 export class TwodsixBattleSheet extends TwodsixShipSheet {
   /** @override */
@@ -18,11 +19,14 @@ export class TwodsixBattleSheet extends TwodsixShipSheet {
 
     //Build Position Data context
     const positionData = [];
-    for (const position of this.actor.itemTypes.ship_position) {
+    const allPositions = this.actor.itemTypes.ship_position.sort(comparePositions);
+    for (const position of allPositions) {
       const actions = {};
       let defaultAction = "";
       for (const action in position.system.actions) {
+        //const actionIcon = getActionIcon(position.system.actions[action].type);
         Object.assign(actions, {[action]: position.system.actions[action].name});
+        //Object.assign(actions, {[action]: [`${position.system.actions[action].name} `, actionIcon]});
         if (!defaultAction) {
           defaultAction = action;
         }
@@ -78,3 +82,22 @@ export class TwodsixBattleSheet extends TwodsixShipSheet {
     }
   }
 }
+
+function comparePositions(a,b) {
+  return a.system.order - b.system.order;
+}
+
+/*function getActionIcon(actionType) {
+  switch (actionType) {
+    case TWODSIX.SHIP_ACTION_TYPE.skillRoll:
+      return `<i class="fa-solid fa-dice-d6"></i>`;
+    case TWODSIX.SHIP_ACTION_TYPE.chatMessage:
+      return `<i class="fa-solid fa-comment"></i>`;
+    case TWODSIX.SHIP_ACTION_TYPE.fireEnergyWeapons:
+      return `<i class="fa-solid fa-crosshairs"></i>`;
+    case TWODSIX.SHIP_ACTION_TYPE.executeMacro:
+      return `<i class="fa-solid fa-code"></i>`;
+    default:
+      return "";
+  }
+}*/
