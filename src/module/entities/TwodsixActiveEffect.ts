f170f0382758120359d52988b9c132f4d69707b8
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2024-09-19 12:31:57 -0500

    fix: AE through macros can trigger encumbrance check (#1634)
    This update fixes an issue with encumbrance status not changing when applying Active Effects through macros. Thanks to Vespilo for finding.

--- src/module/entities/TwodsixActiveEffect.ts
+++ src/module/entities/TwodsixActiveEffect.ts
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
 
+import { applyEncumberedEffect } from "../utils/showStatusIcons";
+
 /**
  * The system-side TwodsixActiveEffect document which overrides/extends the common ActiveEffect model.
  * We extend to our own class to have isSuppressed getter work with equipped status and
- * include a CUSTOM calulation mode directly rather than in a hook
+ * check for encumbrance when an AE is created or deleted.  CUSTOM mode is still applied as a hook.
  * Each TwodsixActiveEffect belongs to the effects collection of its parent Document.
  * Each TwodsixActiveEffect contains a ActiveEffectData object which provides its source data.
  */
@@ -23,5 +25,50 @@ export class TwodsixActiveEffect extends ActiveEffect {
       }
     }
     return false;
+  }
+  /**
+   * Perform follow-up operations after a Document of this type is created.
+   * Post-creation operations occur for all clients after the creation is broadcast.
+   * @param {object} data               The initial data object provided to the document creation request
+   * @param {object} options            Additional options which modify the creation request
+   * @param {string} userId             The id of the User requesting the document update
+   * @see {Document#_onCreate}
+   * @protected
+   */
+  async _onCreate(data, options, userId): void {
+    await super._onCreate(data, options, userId);
+    if(game.userId === userId  && this.parent?.type === 'traveller') {
+      await checkEncumbranceStatus(this);
+    }
+  }
+
+  /**
+   * Perform follow-up operations after a Document of this type is deleted.
+   * Post-deletion operations occur for all clients after the deletion is broadcast.
+   * @param {object} options            Additional options which modify the deletion request
+   * @param {string} userId             The id of the User requesting the document update
+   * @see {Document#_onDelete}
+   * @protected
+   */
+  async _onDelete(options, userId): void {
+    await super._onDelete(options, userId);
+    if(game.userId === userId && this.parent?.type === 'traveller') {
+      await checkEncumbranceStatus(this);
+    }
+
+  }
+}
+
+async function checkEncumbranceStatus (activeEffect:TwodsixActiveEffect):void {
+  if (game.settings.get('twodsix', 'useEncumbranceStatusIndicators')) {
+    if (activeEffect.statuses.size === 0) {
+      await applyEncumberedEffect(activeEffect.parent);
+    } else {
+      const notEncumbered= !activeEffect.statuses.has('encumbered');
+      const notWounded = !activeEffect.statuses.has('wounded');
+      if (notEncumbered && notWounded) {
+        await applyEncumberedEffect(activeEffect.parent);
+      }
+    }
   }
 }
