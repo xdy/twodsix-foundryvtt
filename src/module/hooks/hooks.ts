46cea84b2f6ed7858594caee26f360f741abc05c
marvin9257 <72580196+marvin9257@users.noreply.github.com>
2025-10-05 08:32:42 -0400

    feat: Add optional healing dialog to chat context menu (#1864)
    This release adds an optional popup dialog when "Apply Healing" chat context is used.  If the (renamed) a setting "Auto Damage and Heal Targets" is set to false, a dialog window will open that allows the owner to adjust how healing is applied to each damage stat.  Thanks to Muji-k for the suggestion.

--- src/module/hooks/hooks.ts
+++ src/module/hooks/hooks.ts
@@ -2,6 +2,9 @@
 // @ts-nocheck This turns off *all* typechecking, make sure to remove this once foundry-vtt-types are updated to cover v10.
 
 import { renderDamageDialog, destroyDamageDialog } from "../utils/actorDamage";
+import { destroyHealingDialog, renderHealingDialog } from "../utils/actorHealing";
 
 Hooks.on('createDamageDialog', renderDamageDialog);
 Hooks.on('destroyDamageDialog', destroyDamageDialog);
+Hooks.on('createHealingDialog', renderHealingDialog);
+Hooks.on('destroyHealingDialog', destroyHealingDialog);
